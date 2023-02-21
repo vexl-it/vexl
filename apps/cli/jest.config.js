33f09280f401b130a57810f0fdf6090fa6bdbf09
David Novák <kaladivo@gmail.com>
2023-02-21 12:28:04 +0100

    Remove cli app - for now

--- apps/cli/jest.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  setupFiles: ['dotenv/config'],
-  roots: ['./src'],
-}
--- apps/cli/src/utils/resolveFilePath.ts
+++
@@ -1,8 +0,0 @@
-import {resolve} from 'node:path'
-
-// For development purposes, it is nice to be able to set cwd - when running yarn dev process.cwd() resolves to the root of the project.
-const CURRENT_DIR = process.env.CWD || process.cwd()
-
-export default function resolveFilePath(path?: string): string {
-  return resolve(CURRENT_DIR, path)
-}

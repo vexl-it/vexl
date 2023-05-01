7ff3a2063e5705237492aa2bb2dfe22094d71c64
Davenov <mail@davenov.com>
2023-05-01 23:01:48 +0200

    Add correct version to debug screen

--- apps/mobile/src/utils/environment.ts
+++ apps/mobile/src/utils/environment.ts
@@ -4,3 +4,4 @@ import Constants from 'expo-constants'
 //   Constants.expoConfig?.extra?.enableHiddenFeatures === true
 export const enableHiddenFeatures = false
 export const apiPreset = String(Constants.expoConfig?.extra?.apiPreset)
+export const version = String(Constants.expoConfig?.extra?.version ?? 'local')

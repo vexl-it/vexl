ef52361ecc5be47fc2f04115e0b3ddd0173e1485
David Novák <mail@davenov.com>
2023-06-05 14:05:31 +0200

    fix: #197 - remove countdown in messages

--- apps/mobile/src/components/FromNowComponent.tsx
+++ apps/mobile/src/components/FromNowComponent.tsx
@@ -2,19 +2,29 @@ import {type DateTime} from 'luxon'
 import i18n from '../utils/localization/i18n'
 import {useEffect, useState} from 'react'
 import {useNavigation} from '@react-navigation/native'
+import {
+  type TFunction,
+  useTranslation,
+} from '../utils/localization/I18nProvider'
 
 function whenShouldIUpdateNext(difNowSec: number): number | null {
-  if (difNowSec < 60) {
-    return 1000
-  } else if (difNowSec < 60 * 60) {
+  if (difNowSec < 60 * 60) {
     return 1000 * 60
   }
 
   return null
 }
 
+function getTimeToShow(date: DateTime, t: TFunction): string {
+  if (date.diffNow().as('seconds') < 60) {
+    return t('common.now')
+  }
+  return date.toRelative({style: 'narrow', locale: i18n.locale}) ?? ''
+}
+
 function FromNowComponent({date}: {date: DateTime}): JSX.Element {
   const [toShow, setToShow] = useState('')
+  const {t} = useTranslation()
   const navigation = useNavigation()
 
   useEffect(() => {
@@ -22,15 +32,12 @@ function FromNowComponent({date}: {date: DateTime}): JSX.Element {
 
     let timeoutId: NodeJS.Timeout | null = null
 
-    setToShow(
-      targetDate.toRelative({style: 'narrow', locale: i18n.locale}) ?? ''
-    )
+    setToShow(getTimeToShow(targetDate, t))
 
     function setupUpdate(): void {
       timeoutId && clearTimeout(timeoutId)
-      setToShow(
-        targetDate.toRelative({style: 'narrow', locale: i18n.locale}) ?? ''
-      )
+      setToShow(getTimeToShow(targetDate, t))
+
       const nextUpdateIn = whenShouldIUpdateNext(
         Math.abs(targetDate.diffNow(['seconds']).seconds)
       )
@@ -54,7 +61,7 @@ function FromNowComponent({date}: {date: DateTime}): JSX.Element {
       removeBlurListener()
       timeoutId && clearTimeout(timeoutId)
     }
-  }, [date, navigation, setToShow])
+  }, [date, navigation, setToShow, t])
 
   return <>{toShow}</>
 }

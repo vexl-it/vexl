c07b1e89ec2464e3a469c085fa39ca4c13084e32
David Novák <mail@davenov.com>
2023-06-06 09:11:45 +0200

    fix: #210 - animation should be slide in everywhere

--- apps/mobile/src/components/AnimatedModal.tsx
+++ apps/mobile/src/components/AnimatedModal.tsx
@@ -1,9 +1,9 @@
 import Animated, {
   type BaseAnimationBuilder,
-  BounceIn,
-  BounceOut,
   type EntryExitAnimationFunction,
   type Keyframe,
+  SlideInDown,
+  SlideOutDown,
 } from 'react-native-reanimated'
 import {type ReactNode} from 'react'
 import {Stack} from 'tamagui'
@@ -47,8 +47,8 @@ function AnimatedModal({
   if (shown === false) return null
   return (
     <Animated.View
-      entering={entering ?? BounceIn}
-      exiting={exiting ?? BounceOut}
+      entering={entering ?? SlideInDown}
+      exiting={exiting ?? SlideOutDown}
       style={[styles.root, {top: topMargin}]}
     >
       <Stack pb={bottomInset} f={1}>

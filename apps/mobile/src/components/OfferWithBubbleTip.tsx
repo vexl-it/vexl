ab77474d20a8f69d4ed64812d6ab1aec23e78533
David Novák <mail@davenov.com>
2023-06-09 18:24:20 +0200

    Feature: make offer info pressable on the offer list (not just the button)

--- apps/mobile/src/components/OfferWithBubbleTip.tsx
+++ apps/mobile/src/components/OfferWithBubbleTip.tsx
@@ -4,27 +4,36 @@ import SvgImage from './Image'
 import bubbleTipSvg, {
   bubbleTipSvgNegative,
 } from './InsideRouter/components/MarketplaceScreen/images/bubbleTipSvg'
-import {type ReactNode} from 'react'
+import {type ReactNode, useCallback} from 'react'
 import {type OneOfferInState} from '../state/marketplace/domain'
 import OfferAuthorAvatar from './OfferAuthorAvatar'
+import {TouchableWithoutFeedback} from 'react-native'
 
 export default function OfferWithBubbleTip({
   offer,
   button,
   negative,
+  onInfoRectPress,
 }: {
   offer: OneOfferInState
   button?: ReactNode
   negative?: boolean
+  onInfoRectPress?: () => void
 }): JSX.Element {
+  const onPress = useCallback(() => {
+    if (onInfoRectPress) onInfoRectPress()
+  }, [onInfoRectPress])
+
   return (
     <Stack>
-      <Stack bg={negative ? '$grey' : '$white'} p="$4" br="$5">
-        <OfferInfoPreview negative={negative} offer={offer.offerInfo} />
-        <Stack pos="absolute" b={-7} l={43}>
-          <SvgImage source={negative ? bubbleTipSvgNegative : bubbleTipSvg} />
+      <TouchableWithoutFeedback onPress={onPress}>
+        <Stack bg={negative ? '$grey' : '$white'} p="$4" br="$5">
+          <OfferInfoPreview negative={negative} offer={offer.offerInfo} />
+          <Stack pos="absolute" b={-7} l={43}>
+            <SvgImage source={negative ? bubbleTipSvgNegative : bubbleTipSvg} />
+          </Stack>
         </Stack>
-      </Stack>
+      </TouchableWithoutFeedback>
       <XStack ai="center" jc="space-between" mt="$2">
         <OfferAuthorAvatar offer={offer} negative={negative ?? false} />
         {button && button}

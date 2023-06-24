b9b1564b2cf390778ec7a8b718611633d1511275
Samuel Tremko <samuel@usertechnologies.com>
2023-06-24 18:16:21 +0200

    Allow notifications permissions screen redesign

--- apps/mobile/src/components/NotificationPermissionsScreen/index.tsx
+++ apps/mobile/src/components/NotificationPermissionsScreen/index.tsx
@@ -1,29 +1,58 @@
-import {Text} from 'tamagui'
+import {Stack, Text, XStack} from 'tamagui'
 import WhiteContainer from '../WhiteContainer'
 import Button from '../Button'
 import {useRequestNotificationPermissions} from '../../utils/notifications'
 import useSafeGoBack from '../../utils/useSafeGoBack'
+import SvgImage from '../Image'
+import NotificationsSvg from '../../images/notificationsSvg'
+import {useTranslation} from '../../utils/localization/I18nProvider'
+import Screen from '../Screen'
 
 export function NotificationPermissionsScreen(): JSX.Element {
+  const {t} = useTranslation()
   const safeGoBack = useSafeGoBack()
   const requestNotificationPermissions = useRequestNotificationPermissions()
-  return (
-    <WhiteContainer>
-      <Text mt={16} fos={18} col="$black">
-        Enable notifications for a seamless social experience, as you will
-        instantly receive chat messages. Without notifications enabled your
-        offers will not be automatically encrypted for new users in your
-        contacts.
-      </Text>
 
-      <Button
-        onPress={() => {
-          void requestNotificationPermissions()
-        }}
-        variant={'secondary'}
-        text={'Allow permissions'}
-      />
-      <Button onPress={safeGoBack} variant={'primary'} text={'Go back'} />
-    </WhiteContainer>
+  return (
+    <Screen>
+      <WhiteContainer>
+        <Stack mb={'$3'} f={1} ai="center" jc="center">
+          <SvgImage
+            style={{height: '100%', flex: 1}}
+            source={NotificationsSvg}
+          />
+        </Stack>
+        <Text
+          adjustsFontSizeToFit
+          numberOfLines={2}
+          fos={24}
+          ff="$heading"
+          mt="$4"
+        >
+          {t('postLoginFlow.allowNotifications.title')}
+        </Text>
+        <Text mt={16} fos={18} col="$greyOnWhite">
+          {`${t('postLoginFlow.allowNotifications.text')} ${t(
+            'postLoginFlow.allowNotifications.vexlCantBeUsedWithoutNotifications'
+          )}`}
+        </Text>
+      </WhiteContainer>
+      <XStack space={'$2'} mb={'$2'} mt={'$4'}>
+        <Button
+          fullSize
+          onPress={() => {
+            void requestNotificationPermissions()
+          }}
+          variant={'secondary'}
+          text={t('common.allow')}
+        />
+        <Button
+          fullSize
+          onPress={safeGoBack}
+          variant={'primary'}
+          text={t('common.back')}
+        />
+      </XStack>
+    </Screen>
   )
 }

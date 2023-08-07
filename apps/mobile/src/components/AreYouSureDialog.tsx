4306cbe95b24594a5fb8adf13c795e69ac2c0b34
Samuel Tremko <samuel@usertechnologies.com>
2023-08-07 13:46:03 +0200

    Custom name input for added contact is avoiding keyboard

--- apps/mobile/src/components/AreYouSureDialog.tsx
+++ apps/mobile/src/components/AreYouSureDialog.tsx
@@ -19,6 +19,7 @@ import {useFocusEffect} from '@react-navigation/native'
 import React from 'react'
 import {ImageUniversal, type ImageUniversalSourcePropType} from './Image'
 import Input, {type Props as VexlTextInputProps} from './Input'
+import KeyboardAvoidingView from './KeyboardAvoidingView'
 
 interface StepWithText {
   type: 'StepWithText'
@@ -139,109 +140,111 @@ function AreYouSureDialog(): JSX.Element | null {
 
   return (
     <Stack position={'absolute'} t={0} l={0} r={0} b={0}>
-      <Animated.View
-        entering={FadeIn}
-        exiting={FadeOut}
-        style={styles.backdrop}
-      />
-      <Animated.View
-        style={styles.bounce}
-        entering={SlideInDown}
-        exiting={SlideOutDown}
-      >
-        <YStack
-          pt={insets.top}
-          pl={insets.left}
-          pr={insets.right}
-          pb={insets.bottom}
-          flex={1}
-          space="$2"
+      <KeyboardAvoidingView>
+        <Animated.View
+          entering={FadeIn}
+          exiting={FadeOut}
+          style={styles.backdrop}
+        />
+        <Animated.View
+          style={styles.bounce}
+          entering={SlideInDown}
+          exiting={SlideOutDown}
         >
-          <ScrollView style={styles.flip}>
-            <View style={styles.flip}>
-              <Stack px={'$4'} br={'$4'} mx={'$2'} py="$5" bc={'$white'}>
-                {step.type === 'StepWithText' ? (
-                  <>
-                    {step.image && (
-                      <ImageUniversal
-                        style={{maxWidth: '100%'}}
-                        source={step.image}
-                      />
-                    )}
-                    <Text
-                      fontFamily={'$heading'}
-                      fontSize={32}
-                      color={'$black'}
-                    >
-                      {step.title}
-                    </Text>
-                    {step.description && (
-                      <Text fontSize={18} color={'$greyOnWhite'}>
+          <YStack
+            pt={insets.top}
+            pl={insets.left}
+            pr={insets.right}
+            pb={insets.bottom}
+            flex={1}
+            space="$2"
+          >
+            <ScrollView style={styles.flip}>
+              <View style={styles.flip}>
+                <Stack px={'$4'} br={'$4'} mx={'$2'} py="$5" bc={'$white'}>
+                  {step.type === 'StepWithText' ? (
+                    <>
+                      {step.image && (
+                        <ImageUniversal
+                          style={{maxWidth: '100%'}}
+                          source={step.image}
+                        />
+                      )}
+                      <Text
+                        fontFamily={'$heading'}
+                        fontSize={32}
+                        color={'$black'}
+                      >
+                        {step.title}
+                      </Text>
+                      {step.description && (
+                        <Text fontSize={18} color={'$greyOnWhite'}>
+                          {step.description}
+                        </Text>
+                      )}
+                    </>
+                  ) : step.type === 'StepWithInput' ? (
+                    <Stack space={'$4'}>
+                      <Text fos={28} col={'$black'} ff={'$heading'}>
+                        {step.title}
+                      </Text>
+                      <Text fos={18} col={'$greyOnWhite'} ff={'$body500'}>
                         {step.description}
                       </Text>
-                    )}
-                  </>
-                ) : step.type === 'StepWithInput' ? (
-                  <Stack space={'$4'}>
-                    <Text fos={28} col={'$black'} ff={'$heading'}>
-                      {step.title}
-                    </Text>
-                    <Text fos={18} col={'$greyOnWhite'} ff={'$body500'}>
-                      {step.description}
-                    </Text>
-                    <Text fos={18} col={'$black'} ff={'$heading'}>
-                      {step.subtitle}
-                    </Text>
-                    <Input
-                      value={
-                        state.stepResults[0].type === 'inputResult'
-                          ? state.stepResults[0].value
-                          : undefined
-                      }
-                      onChangeText={(value) => {
-                        setState({
-                          ...state,
-                          stepResults: [{type: 'inputResult', value}],
-                        })
-                      }}
-                      {...step.textInputProps}
-                    />
-                  </Stack>
-                ) : (
-                  step.children
-                )}
-              </Stack>
-            </View>
-          </ScrollView>
-          <XStack space="$2" m="$2" height={60}>
-            {step.negativeButtonText && (
+                      <Text fos={18} col={'$black'} ff={'$heading'}>
+                        {step.subtitle}
+                      </Text>
+                      <Input
+                        value={
+                          state.stepResults[0].type === 'inputResult'
+                            ? state.stepResults[0].value
+                            : undefined
+                        }
+                        onChangeText={(value) => {
+                          setState({
+                            ...state,
+                            stepResults: [{type: 'inputResult', value}],
+                          })
+                        }}
+                        {...step.textInputProps}
+                      />
+                    </Stack>
+                  ) : (
+                    step.children
+                  )}
+                </Stack>
+              </View>
+            </ScrollView>
+            <XStack space="$2" m="$2" height={60}>
+              {step.negativeButtonText && (
+                <Button
+                  fullSize
+                  variant={state.variant === 'danger' ? 'redDark' : 'primary'}
+                  onPress={() => {
+                    state?.onDismiss()
+                    setState(null)
+                  }}
+                  text={step.negativeButtonText}
+                />
+              )}
               <Button
                 fullSize
-                variant={state.variant === 'danger' ? 'redDark' : 'primary'}
                 onPress={() => {
-                  state?.onDismiss()
-                  setState(null)
+                  if (!state) return
+                  if (state.currentStep >= state.steps.length - 1) {
+                    state.onPass(state.stepResults)
+                    setState(null)
+                  } else {
+                    setState({...state, currentStep: state.currentStep + 1})
+                  }
                 }}
-                text={step.negativeButtonText}
+                variant={state.variant === 'danger' ? 'redLight' : 'secondary'}
+                text={step.positiveButtonText}
               />
-            )}
-            <Button
-              fullSize
-              onPress={() => {
-                if (!state) return
-                if (state.currentStep >= state.steps.length - 1) {
-                  state.onPass(state.stepResults)
-                  setState(null)
-                } else {
-                  setState({...state, currentStep: state.currentStep + 1})
-                }
-              }}
-              variant={state.variant === 'danger' ? 'redLight' : 'secondary'}
-              text={step.positiveButtonText}
-            />
-          </XStack>
-        </YStack>
-      </Animated.View>
+            </XStack>
+          </YStack>
+        </Animated.View>
+      </KeyboardAvoidingView>
     </Stack>
   )
 }

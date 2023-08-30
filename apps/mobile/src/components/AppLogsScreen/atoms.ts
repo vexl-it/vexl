b966f3a4705de8aec5bb12926c133c4016d1f06f
AdamSchinzel <schinzel.adam@gmail.com>
2023-08-30 14:47:05 +0200

    fix: move atoms outside of component

--- apps/mobile/src/components/AppLogsScreen/atoms.ts
+++ apps/mobile/src/components/AppLogsScreen/atoms.ts
@@ -1,6 +1,6 @@
 import {atom} from 'jotai'
 import {listenOnAppLogs, readLogs} from './utils/storage'
-import {splitAtom} from 'jotai/utils'
+import {selectAtom, splitAtom} from 'jotai/utils'
 
 const appLogsAtom = atom<string[]>([])
 appLogsAtom.onMount = (setAtom) => {
@@ -10,5 +10,7 @@ appLogsAtom.onMount = (setAtom) => {
 }
 
 export const appLogAtomsAtom = splitAtom(appLogsAtom)
+export const appLogsEmptyAtom = selectAtom(appLogAtomsAtom, logs => logs.length === 0)
 
 export default appLogsAtom
+

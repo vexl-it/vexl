2080041804e2cf7452329aaeb4edfa27493ea977
David Novák <mail@davenov.com>
2023-09-29 08:02:31 +0200

    fix: #224 - deduplicate contacts in contactList

--- apps/mobile/src/components/ContactListSelect/index.tsx
+++ apps/mobile/src/components/ContactListSelect/index.tsx
@@ -43,7 +43,6 @@ function ContactsListSelect({
 
   useOnFocusAndAppState(
     useCallback(() => {
-      console.log('triggering')
       triggerContactsReload()
     }, [triggerContactsReload])
   )

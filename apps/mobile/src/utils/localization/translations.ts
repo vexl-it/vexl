b159385d52179aa8d0ae4ed783f00e11ed505d7b
David Novák <mail@davenov.com>
2023-11-06 14:04:04 +0100

    feat: Setup crowdin

---
+++ apps/mobile/src/utils/localization/translations.ts
@@ -0,0 +1,89 @@
+import base from '@vexl-next/localization/base.json'
+import privacyPolicyDev from '@vexl-next/localization/privacyPolicy.json'
+import termsOfUse from '@vexl-next/localization/termsOfUse.json'
+
+import csBase from '@vexl-next/localization/cs-base.json'
+import csPrivacyPolicyDev from '@vexl-next/localization/cs-privacyPolicy.json'
+import csTermsOfUse from '@vexl-next/localization/cs-termsOfUse.json'
+
+import deBase from '@vexl-next/localization/de-base.json'
+import dePrivacyPolicyDev from '@vexl-next/localization/de-privacyPolicy.json'
+import deTermsOfUse from '@vexl-next/localization/de-termsOfUse.json'
+
+import enBase from '@vexl-next/localization/en-base.json'
+import enPrivacyPolicyDev from '@vexl-next/localization/en-privacyPolicy.json'
+import enTermsOfUse from '@vexl-next/localization/en-termsOfUse.json'
+
+import frBase from '@vexl-next/localization/fr-base.json'
+import frPrivacyPolicyDev from '@vexl-next/localization/fr-privacyPolicy.json'
+import frTermsOfUse from '@vexl-next/localization/fr-termsOfUse.json'
+
+import itBase from '@vexl-next/localization/it-base.json'
+import itPrivacyPolicyDev from '@vexl-next/localization/it-privacyPolicy.json'
+import itTermsOfUse from '@vexl-next/localization/it-termsOfUse.json'
+
+import ptBase from '@vexl-next/localization/pt-base.json'
+import ptPrivacyPolicyDev from '@vexl-next/localization/pt-privacyPolicy.json'
+import ptTermsOfUse from '@vexl-next/localization/pt-termsOfUse.json'
+
+import esBase from '@vexl-next/localization/es-base.json'
+import esPrivacyPolicyDev from '@vexl-next/localization/es-privacyPolicy.json'
+import esTermsOfUse from '@vexl-next/localization/es-termsOfUse.json'
+
+import skBase from '@vexl-next/localization/sk-base.json'
+import skPrivacyPolicyDev from '@vexl-next/localization/sk-privacyPolicy.json'
+import skTermsOfUse from '@vexl-next/localization/sk-termsOfUse.json'
+
+export const dev = {
+  ...base,
+  termsOfUseMD: termsOfUse,
+  privacyPolicyMD: privacyPolicyDev,
+} as const
+
+export const cs = {
+  ...csBase,
+  termsOfUseMD: csTermsOfUse,
+  privacyPolicyMD: csPrivacyPolicyDev,
+} as const
+
+export const de = {
+  ...deBase,
+  termsOfUseMD: deTermsOfUse,
+  privacyPolicyMD: dePrivacyPolicyDev,
+} as const
+
+export const en = {
+  ...enBase,
+  termsOfUseMD: enTermsOfUse,
+  privacyPolicyMD: enPrivacyPolicyDev,
+} as const
+
+export const fr = {
+  ...frBase,
+  termsOfUseMD: frTermsOfUse,
+  privacyPolicyMD: frPrivacyPolicyDev,
+} as const
+
+export const it = {
+  ...itBase,
+  termsOfUseMD: itTermsOfUse,
+  privacyPolicyMD: itPrivacyPolicyDev,
+} as const
+
+export const pt = {
+  ...ptBase,
+  termsOfUseMD: ptTermsOfUse,
+  privacyPolicyMD: ptPrivacyPolicyDev,
+} as const
+
+export const es = {
+  ...esBase,
+  termsOfUseMD: esTermsOfUse,
+  privacyPolicyMD: esPrivacyPolicyDev,
+} as const
+
+export const sk = {
+  ...skBase,
+  termsOfUseMD: skTermsOfUse,
+  privacyPolicyMD: skPrivacyPolicyDev,
+} as const
--- packages/localizations/src/.eslintrc.cjs
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  "rules": {
-    "quote-props": ["error", "always"],
-    "comma-dangle": ["error", "never"],
-    "semi": ["error", "never"]
-  }
-};

acc87dede7c4fab83e2c2e6d764a47e4167c4c44
David Novák <mail@davenov.com>
2023-11-22 12:36:26 +0100

    feat/#595 - Dev translate mode

---
+++ apps/mobile/src/components/DevTranslationFloatingButtons.tsx
@@ -0,0 +1,25 @@
+import {useSetAtom} from 'jotai'
+import {Dimensions} from 'react-native'
+import Draggable from 'react-native-draggable'
+import {getTokens} from 'tamagui'
+import {showDevLabelsAtom} from '../utils/localization/I18nProvider'
+
+function DevTranslationFloatingButton(): JSX.Element {
+  const setShowDevLabels = useSetAtom(showDevLabelsAtom)
+  return (
+    <Draggable
+      x={Dimensions.get('window').width - 100}
+      y={Dimensions.get('window').height - 100}
+      z={1000}
+      renderSize={56}
+      renderColor={getTokens().color.red.val}
+      renderText={'🔍'}
+      isCircle
+      onShortPressRelease={() => {
+        setShowDevLabels((prev) => !prev)
+      }}
+    />
+  )
+}
+
+export default DevTranslationFloatingButton

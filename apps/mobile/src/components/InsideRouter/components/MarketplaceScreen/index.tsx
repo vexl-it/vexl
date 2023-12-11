cbeb579d9f638e1f9b51dfdce431f7d7d2f1add0
Samuel Tremko <samuel@usertechnologies.com>
2023-12-11 08:37:39 +0100

    feat: #630 - Automatic buy/sell resizing

--- apps/mobile/src/components/InsideRouter/components/MarketplaceScreen/index.tsx
+++ apps/mobile/src/components/InsideRouter/components/MarketplaceScreen/index.tsx
@@ -2,53 +2,29 @@ import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs
 import {OfferType} from '@vexl-next/domain/dist/general/offers'
 import {useSetAtom} from 'jotai'
 import {useCallback, useMemo} from 'react'
-import {getTokens, useMedia} from 'tamagui'
+import {getTokens} from 'tamagui'
 import {type MarketplaceTabParamsList} from '../../../../navigationTypes'
 import {triggerOffersRefreshAtom} from '../../../../state/marketplace'
-import {
-  getCurrentLocale,
-  useTranslation,
-} from '../../../../utils/localization/I18nProvider'
+import {useTranslation} from '../../../../utils/localization/I18nProvider'
 import {useAppState} from '../../../../utils/useAppState'
 import ContainerWithTopBorderRadius, {
   CONTAINER_WITH_TOP_BORDER_RADIUS_TOP_PADDING,
 } from '../ContainerWithTopBorderRadius'
 import OffersListWithFilter from './components/OffersListStateDisplayer'
+import CustomTabBar from './components/CustomTabBar'
 
 const Tab = createMaterialTopTabNavigator<MarketplaceTabParamsList>()
 
-const tabsFontSizes: Record<string, {sm: number; md: number; lg: number}> = {
-  default: {
-    sm: 30,
-    md: 40,
-    lg: 40,
-  },
-  de: {sm: 20, md: 20, lg: 25},
-  cs: {sm: 25, md: 30, lg: 30},
-  sk: {sm: 25, md: 30, lg: 30},
-}
-
 function MarketplaceScreen(): JSX.Element {
   const {t} = useTranslation()
-  const media = useMedia()
   const tokens = getTokens()
 
   const {
     tabBarStyle,
     tabBarContentContainerStyle,
-    tabBarLabelStyle,
+    // tabBarLabelStyle,
     tabBarIndicatorStyle,
   } = useMemo(() => {
-    const tabLabelFontSize = (() => {
-      const sizesForLangauge =
-        tabsFontSizes[getCurrentLocale().split('-').at(0) ?? 'default'] ??
-        tabsFontSizes.default
-
-      if (media.sm) return sizesForLangauge?.sm
-      if (media.md) return sizesForLangauge?.md
-      if (media.lg) return sizesForLangauge?.lg
-    })()
-
     return {
       tabBarStyle: {
         marginTop: CONTAINER_WITH_TOP_BORDER_RADIUS_TOP_PADDING,
@@ -60,18 +36,12 @@ function MarketplaceScreen(): JSX.Element {
         margin: 0,
         padding: 0,
       },
-      tabBarLabelStyle: {
-        fontSize: tabLabelFontSize,
-        fontFamily: 'PPMonument',
-        textTransform: 'none',
-        margin: 0,
-      },
       tabBarIndicatorStyle: {
         height: 2,
         bottom: -2,
       },
     } as const
-  }, [tokens.color.black.val, tokens.color.grey.val, media])
+  }, [tokens.color.black.val, tokens.color.grey.val])
 
   const refreshOffers = useSetAtom(triggerOffersRefreshAtom)
 
@@ -89,10 +59,10 @@ function MarketplaceScreen(): JSX.Element {
   return (
     <ContainerWithTopBorderRadius>
       <Tab.Navigator
+        tabBar={CustomTabBar}
         screenOptions={{
           tabBarStyle,
           tabBarContentContainerStyle,
-          tabBarLabelStyle,
           tabBarActiveTintColor: tokens.color.main.val,
           tabBarInactiveTintColor: tokens.color.greyOnBlack.val,
           tabBarIndicatorStyle,

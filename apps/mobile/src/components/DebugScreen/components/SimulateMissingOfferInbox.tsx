8e5b06d8bb5a95d707fd7381a59ed5bc3d1429d4
David Novák <mail@davenov.com>
2024-01-23 08:30:45 +0100

    feat: add debug function for bulk creating offers

--- apps/mobile/src/components/DebugScreen/components/SimulateMissingOfferInbox.tsx
+++ apps/mobile/src/components/DebugScreen/components/SimulateMissingOfferInbox.tsx
@@ -1,12 +1,20 @@
 import {Picker} from '@react-native-picker/picker'
 import {type OneOfferInState} from '@vexl-next/domain/src/general/offers'
+import {generateKeyPair} from '@vexl-next/resources-utils/src/utils/crypto'
+import * as A from 'fp-ts/Array'
+import * as T from 'fp-ts/Task'
+import * as TE from 'fp-ts/TaskEither'
+import {pipe} from 'fp-ts/lib/function'
 import {useAtomValue, useStore} from 'jotai'
 import {useState} from 'react'
 import {Alert} from 'react-native'
 import {Text, YStack} from 'tamagui'
 import {usePrivateApiAssumeLoggedIn} from '../../../api'
 import messagingStateAtom from '../../../state/chat/atoms/messagingStateAtom'
+import {createInboxAtom} from '../../../state/chat/hooks/useCreateInbox'
+import {createOfferAtom} from '../../../state/marketplace'
 import {myOffersAtom} from '../../../state/marketplace/atoms/myOffers'
+import {packageName} from '../../../utils/environment'
 import Button from '../../Button'
 
 function SimulateMissingOfferInbox(): JSX.Element {
@@ -26,6 +34,63 @@ function SimulateMissingOfferInbox(): JSX.Element {
       return
     }
     await api.chat.deleteInbox({keyPair: inbox.inbox.privateKey})()
+  }
+
+  function cloneOffer10x(offer: OneOfferInState): T.Task<null> {
+    if (packageName === 'it.vexl.next') {
+      Alert.alert('Not available in production')
+      return T.of(null)
+    }
+    return pipe(
+      Array(10).fill(0),
+      A.mapWithIndex((i) =>
+        pipe(
+          generateKeyPair(),
+          TE.fromEither,
+          TE.bindTo('key'),
+          TE.bindW('createdOffer', ({key}) =>
+            store.set(createOfferAtom, {
+              payloadPublic: {
+                ...offer.offerInfo.publicPart,
+                offerPublicKey: key.publicKeyPemBase64,
+                offerDescription: `#${i} ${offer.offerInfo.publicPart.offerDescription}`,
+              },
+              intendedConnectionLevel:
+                offer.ownershipInfo?.intendedConnectionLevel ?? 'FIRST',
+              onProgress: (progress) => {
+                console.log(
+                  `Creating offer ${i + 1}/ 10, progress: ${JSON.stringify(
+                    progress
+                  )}`
+                )
+              },
+            })
+          ),
+          TE.chainFirstW(({key, createdOffer}) =>
+            store.set(createInboxAtom, {
+              inbox: {
+                privateKey: key,
+                offerId: createdOffer.offerInfo.offerId,
+              },
+            })
+          ),
+          TE.matchW(
+            (l) => {
+              console.error(
+                `Creating offer ${
+                  i + 1
+                }/ 10, Error creating offer: ${JSON.stringify(l)}`
+              )
+            },
+            () => {
+              console.log(`created offer ${i + 1}/ 10`)
+            }
+          )
+        )
+      ),
+      T.sequenceSeqArray,
+      T.map(() => null)
+    )
   }
 
   function removeInboxFromState(forOffer: OneOfferInState): void {
@@ -72,6 +137,19 @@ function SimulateMissingOfferInbox(): JSX.Element {
           })
         }}
         text="Delete everywhere"
+      />
+      <Button
+        size="small"
+        variant="primary"
+        disabled={!selectedOffer}
+        onPress={() => {
+          if (!selectedOffer) return
+          void cloneOffer10x(selectedOffer)().then(() => {
+            removeInboxFromState(selectedOffer)
+            Alert.alert('Done')
+          })
+        }}
+        text="Clone offer 10x"
       />
     </YStack>
   )

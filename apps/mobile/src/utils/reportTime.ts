bee8480ec4480f69c2b6b68550e6f148bd655925
David Novák <mail@davenov.com>
2024-01-24 11:29:09 +0100

    feature: add sentry instrumentation

--- apps/mobile/src/utils/reportTime.ts
+++ apps/mobile/src/utils/reportTime.ts
@@ -1,11 +1,16 @@
+import {startInactiveSpan} from '@sentry/react-native'
+
 const numberFormatIntl = new Intl.NumberFormat('cs', {})
 
 export function startMeasure(name: string): (millisec?: boolean) => string {
   const start = Date.now()
+  // TODO #728 use active spans to track errors in spans
+  // https://docs.sentry.io/platforms/react-native/performance/instrumentation/custom-instrumentation/?original_referrer=https%3A%2F%2Fwww.google.com%2F#adding-span-operations
+  const span = startInactiveSpan({name})
 
   return (millisec) => {
     const end = Date.now()
-    // TODO log to server?
+    span?.finish(end)
 
     const prettyDuration = numberFormatIntl.format(
       millisec ? end - start : (end - start) / 1000

89175125e029e8df701e0cbe73f7335e99f7a229
David Novák <mail@davenov.com>
2024-01-24 07:00:31 +0100

    feat: #328 - warn when registering an existing user

--- packages/rest-api/src/services/contact/index.ts
+++ packages/rest-api/src/services/contact/index.ts
@@ -23,6 +23,7 @@ import {
   type RefreshUserRequest,
   type UpdateFirebaseTokenRequest,
   type UserNotFoundError,
+  UserExistsResponse,
 } from './contracts'
 
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type

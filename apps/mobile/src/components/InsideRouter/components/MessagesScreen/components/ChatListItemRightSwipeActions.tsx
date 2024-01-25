cfc78cfb75ac47b5a2cefc9d8980e1ba3769f109
Samuel Tremko <samuel@usertechnologies.com>
2024-01-25 15:18:55 +0100

    Swipe left to delete old chats

---
+++ apps/mobile/src/components/InsideRouter/components/MessagesScreen/components/ChatListItemRightSwipeActions.tsx
@@ -0,0 +1,24 @@
+import {TouchableOpacity, type TouchableOpacityProps} from 'react-native'
+import {Stack, getTokens} from 'tamagui'
+import Image from '../../../../Image'
+import trashIconSvg from '../../SettingsScreen/images/trashIconSvg'
+
+function ChatListItemRightSwipeActions(
+  props: TouchableOpacityProps
+): JSX.Element {
+  return (
+    <TouchableOpacity {...props}>
+      <Stack w={80} h={48} bc="$grey" als="center" ai="center" jc="center">
+        <Image
+          width={32}
+          height={32}
+          stroke={getTokens().color.white.val}
+          fill={getTokens().color.white.val}
+          source={trashIconSvg}
+        />
+      </Stack>
+    </TouchableOpacity>
+  )
+}
+
+export default ChatListItemRightSwipeActions

6639cb697ce5ae94d2ae1bf12ee32334f450ef2d
David Novák <mail@davenov.com>
2024-01-29 18:03:57 +0100

    #739 Cache btc price and fetch it only every 10minutes to prevent server throttling & do not show alert on fail

--- apps/mobile/src/state/currentBtcPriceAtoms.ts
+++ apps/mobile/src/state/currentBtcPriceAtoms.ts
@@ -1,104 +1,96 @@
 import {type BtcPriceDataWithState} from '@vexl-next/domain/src/general/btcPrice'
-import {CurrencyCode} from '@vexl-next/domain/src/general/currency.brand'
+import {type CurrencyCode} from '@vexl-next/domain/src/general/currency.brand'
+import {unixMillisecondsNow} from '@vexl-next/domain/src/utility/UnixMilliseconds.brand'
 import {AcceptedCurrency} from '@vexl-next/rest-api/src/services/btcPrice'
 import {pipe} from 'fp-ts/function'
 import * as T from 'fp-ts/Task'
 import * as TE from 'fp-ts/TaskEither'
 import {atom, type Atom, type PrimitiveAtom} from 'jotai'
 import {publicApiAtom} from '../api'
-import {translationAtom} from '../utils/localization/I18nProvider'
 import reportError from '../utils/reportError'
-import showErrorAlert from '../utils/showErrorAlert'
-import {toCommonErrorMessage} from '../utils/useCommonErrorMessages'
 import {selectedCurrencyAtom} from './selectedCurrency'
 
-const BULGARIAN_LEV_PEGGED_EURO_RATE = 1.95583
-
-const dummyBtcPriceDataState: Record<string, BtcPriceDataWithState> = {
-  [CurrencyCode.parse('USD')]: {
-    btcPrice: 0,
-    state: 'error',
-  },
-}
+const FETCH_LIMIT = 10 * 60 * 1000 // 10 minutes
 
 export const btcPriceDataAtom: PrimitiveAtom<
-  Record<string, BtcPriceDataWithState>
-> = atom(dummyBtcPriceDataState)
+  Partial<Record<CurrencyCode, BtcPriceDataWithState>>
+> = atom({})
 
-export const btcPriceForSelectedCurrencyAtom: Atom<BtcPriceDataWithState> =
-  atom((get) => {
-    const selectedCurrency = get(selectedCurrencyAtom)
+export function createBtcPriceForCurrencyAtom(
+  currencyStringOrAtom: CurrencyCode | Atom<CurrencyCode | undefined>
+): Atom<BtcPriceDataWithState | undefined> {
+  return atom((get): BtcPriceDataWithState | undefined => {
     const btcPriceData = get(btcPriceDataAtom)
 
-    return (
-      btcPriceData[selectedCurrency] ??
-      ({btcPrice: 0, state: 'error'} satisfies BtcPriceDataWithState)
-    )
+    const currency = (() => {
+      if (typeof currencyStringOrAtom === 'string') return currencyStringOrAtom
+      return get(currencyStringOrAtom)
+    })()
+
+    if (!currency) {
+      return undefined
+    }
+
+    return btcPriceData[currency]
   })
+}
+
+export const btcPriceForSelectedCurrencyAtom =
+  createBtcPriceForCurrencyAtom(selectedCurrencyAtom)
 
 export const refreshBtcPriceActionAtom = atom(
   undefined,
   (get, set, currency: CurrencyCode) => {
     const api = get(publicApiAtom)
-    const {t} = get(translationAtom)
 
-    // Bulgarian LEV is pegged to Euro and as CoinGecko does not support it
-    // we calculate it manually from EUR price
-    const acceptedCurrency =
-      currency === 'BGN'
-        ? AcceptedCurrency.safeParse('EUR'.toLowerCase())
-        : AcceptedCurrency.safeParse(currency.toLowerCase())
-
+    // TODO this can be done statically in constant. Should not be parsed every time.
+    const acceptedCurrency = AcceptedCurrency.safeParse(currency.toLowerCase())
     if (!acceptedCurrency.success) return T.of(false)
+
+    const fetchInfo = get(btcPriceDataAtom)[currency]
+    if (
+      fetchInfo?.state === 'success' &&
+      fetchInfo.lastRefreshAt + FETCH_LIMIT > unixMillisecondsNow()
+    ) {
+      return T.of(true)
+    }
 
     set(btcPriceDataAtom, (prevState) => ({
       ...prevState,
       [currency]: {
-        btcPrice: prevState[currency]?.btcPrice ?? 0,
         state: 'loading',
-      },
+        btcPrice: prevState[currency]?.btcPrice ?? undefined,
+      } satisfies BtcPriceDataWithState,
     }))
 
     return pipe(
       api.btcPrice(acceptedCurrency.data),
       TE.matchW(
         (l) => {
-          showErrorAlert({
-            title:
-              toCommonErrorMessage(l, t) ??
-              t('btcPriceChart.requestCouldNotBeProcessed'),
-            error: l,
+          reportError('warn', new Error('Error while fetching btc price'), {
+            l,
           })
-          reportError('warn', new Error('Error while fetching btc price'), {l})
 
           set(btcPriceDataAtom, (prevState) => ({
             ...prevState,
             [currency]: {
               btcPrice: prevState[currency]?.btcPrice ?? 0,
               state: 'error',
+              error: l,
             },
           }))
 
           return false
         },
         (btcPrice) => {
-          if (currency === 'BGN') {
-            set(btcPriceDataAtom, (prevState) => ({
-              ...prevState,
-              [currency]: {
-                btcPrice: Math.round(btcPrice * BULGARIAN_LEV_PEGGED_EURO_RATE),
-                state: 'success',
-              },
-            }))
-          } else {
-            set(btcPriceDataAtom, (prevState) => ({
-              ...prevState,
-              [currency]: {
-                btcPrice,
-                state: 'success',
-              },
-            }))
-          }
+          set(btcPriceDataAtom, (prevState) => ({
+            ...prevState,
+            [currency]: {
+              btcPrice,
+              state: 'success',
+              lastRefreshAt: unixMillisecondsNow(),
+            } satisfies BtcPriceDataWithState,
+          }))
 
           return true
         }
--- packages/domain/src/general/btcPrice.ts
+++ packages/domain/src/general/btcPrice.ts
@@ -1,8 +1,24 @@
 import {z} from 'zod'
+import {UnixMilliseconds} from '../utility/UnixMilliseconds.brand'
 
-export const BtcPriceDataWithState = z.object({
-  btcPrice: z.number(),
-  state: z.enum(['loading', 'success', 'error']),
-})
+export const BtcPrice = z.number().positive().brand('BtcPrice')
+export type BtcPrice = z.TypeOf<typeof BtcPrice>
+
+export const BtcPriceDataWithState = z.discriminatedUnion('state', [
+  z.object({
+    state: z.literal('loading'),
+    btcPrice: z.number().optional(),
+  }),
+  z.object({
+    state: z.literal('success'),
+    btcPrice: z.number(),
+    lastRefreshAt: UnixMilliseconds,
+  }),
+  z.object({
+    state: z.literal('error'),
+    btcPrice: z.number().optional(),
+    error: z.unknown(),
+  }),
+])
 
 export type BtcPriceDataWithState = z.TypeOf<typeof BtcPriceDataWithState>

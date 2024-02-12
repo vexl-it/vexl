c3044cd2031ea675979e7819e7e20bbe731d280e
David Novák <mail@davenov.com>
2024-02-12 14:18:25 +0100

    fix: fix unread messages indicator

--- apps/mobile/src/state/chat/atoms/unreadChatsCountAtom.ts
+++ apps/mobile/src/state/chat/atoms/unreadChatsCountAtom.ts
@@ -1,16 +1,20 @@
 import {atom} from 'jotai'
 import {focusAtom} from 'jotai-optics'
 import {selectAtom} from 'jotai/utils'
+import isChatActive from '../utils/isChatActive'
 import messagingStateAtom from './messagingStateAtom'
 
 const chatsListAtom = focusAtom(messagingStateAtom, (optic) =>
-  optic.elems().prop('chats').elems().prop('chat')
+  optic.elems().prop('chats').elems()
 )
 
 export const unreadChatsCountAtom = atom(
-  (get) => get(chatsListAtom).filter((chat) => chat.isUnread).length
+  (get) =>
+    get(chatsListAtom)
+      .filter(isChatActive)
+      .filter((chat) => chat.chat.isUnread).length
 )
 
 export const areThereUnreadMessagesAtom = selectAtom(chatsListAtom, (chats) =>
-  chats.some((one) => one.isUnread)
+  chats.filter(isChatActive).some((one) => one.chat.isUnread)
 )

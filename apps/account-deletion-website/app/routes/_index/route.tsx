7c41181f60a3cf015891d68ed30ed86f8d10a379
leomira <petrasovalea@gmail.com>
2024-03-21 09:22:08 -0600

    Update route.tsx
    minor, minor, minor update in the order of paragraphs

--- apps/account-deletion-website/app/routes/_index/route.tsx
+++ apps/account-deletion-website/app/routes/_index/route.tsx
@@ -9,18 +9,18 @@ export default function index(): JSX.Element {
         to delete your account.
       </p>
 
+       <p>
+        If you lost your phone or uninstalled the app you can remove your
+        account using this page.
+      </p>
+
       <p>
         The best way to delete your account is to click on the{' '}
         <i>delete account</i> button in the app:
       </p>
 
       <img className={classes.image} src="/assets/delete.jpeg"></img>
-
-      <p>
-        If you lost your phone or uninstalled the app you can remove your
-        account using this page.
-      </p>
-
+      
       <Link className="button" to="/deleteAccount1">
         I understand, I want to delete my account here, on the website
       </Link>

7892c0a3a1e79fd30c5fa31dc785faecf5f76f7a
Samuel Tremko <samuel@usertechnologies.com>
2024-04-03 12:17:45 +0200

    Fix app to accept taproot address format

--- packages/domain/src/utility/BtcAddress.brand.ts
+++ packages/domain/src/utility/BtcAddress.brand.ts
@@ -2,6 +2,6 @@ import {z} from 'zod'
 
 export const BtcAddress = z
   .string()
-  .regex(/^(bc1|[13])[a-zA-HJ-NP-Z0-9]{25,39}$/)
+  .regex(/^(bc1|[13])[a-zA-HJ-NP-Z0-9]{25,59}$/)
   .brand<'BtcAddress'>()
 export type BtcAddress = z.TypeOf<typeof BtcAddress>

ebd6bfc7196e2c8521642695e492bb35dcb8eb9a
Samuel Tremko <samuel@usertechnologies.com>
2024-04-09 13:14:55 +0200

    Put offers without listing type first only for my offers

--- apps/mobile/src/state/marketplace/utils/sortOffers.ts
+++ apps/mobile/src/state/marketplace/utils/sortOffers.ts
@@ -10,11 +10,16 @@ export default function sortOffers(
   const toReturn = [...offers]
 
   toReturn.sort((a, b) => {
-    const aHasListingType = !!a.offerInfo.publicPart.listingType
-    const bHasListingType = !!b.offerInfo.publicPart.listingType
+    const aIsMyOffer = !!a.ownershipInfo
+    const bIsMyOffer = !!b.ownershipInfo
 
-    if (!aHasListingType && bHasListingType) return -1
-    if (aHasListingType && !bHasListingType) return 1
+    if (aIsMyOffer && bIsMyOffer) {
+      const aHasListingType = !!a.offerInfo.publicPart.listingType
+      const bHasListingType = !!b.offerInfo.publicPart.listingType
+
+      if (!aHasListingType && bHasListingType) return -1
+      if (aHasListingType && !bHasListingType) return 1
+    }
 
     if (sort === 'LOWEST_FEE_FIRST')
       return a.offerInfo.publicPart.feeAmount - b.offerInfo.publicPart.feeAmount

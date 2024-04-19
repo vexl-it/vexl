58c006166b0325436b863b3b240759d8493f123b
David Novák <mail@davenov.com>
2024-04-19 16:07:05 +0200

    hotfix splashcreen hiding & release 1.15.1 (175)

--- apps/mobile/app.config.ts
+++ apps/mobile/app.config.ts
@@ -1,6 +1,6 @@
 // import {SemverString} from '@vexl-next/domain/src/utility/SmeverString.brand'
 
-const VERSION_CODE = 174
+const VERSION_CODE = 175
 const VERSION = '1.15.1'
 const ENV_PRESET = process.env.ENV_PRESET
 const COMMIT_HASH = process.env.EAS_BUILD_GIT_COMMIT_HASH ?? 'local'

45dcca88bcfb7aefa4712c30b381c24ccb18725a
David Novák <mail@davenov.com>
2024-04-29 13:16:24 +0200

    fix: remove auth from /cypher-public-key endpoint

--- apps/notification-service/src/routes/GetkeyRouteLive.ts
+++ apps/notification-service/src/routes/GetkeyRouteLive.ts
@@ -1,6 +1,5 @@
 import * as Http from '@effect/platform/HttpServer'
 import {GetPublicKeyResponseE} from '@vexl-next/rest-api/src/services/notification/contract'
-import {AuthenticatedSessionInRequestLive} from '@vexl-next/server-utils/src/ServerUserSession'
 import {Effect} from 'effect'
 import {EnvironmentConstants} from '../EnvironmentLayer'
 
@@ -9,8 +8,8 @@ const GetKeyRouteLive = Http.router.get(
   EnvironmentConstants.FCM_TOKEN_PUBLIC_KEY.pipe(
     Effect.flatMap((fcmToken) =>
       Http.response.schemaJson(GetPublicKeyResponseE)({publicKey: fcmToken})
-    ),
-    Effect.provide(AuthenticatedSessionInRequestLive)
+    )
+    // Effect.provide(AuthenticatedSessionInRequestLive)
   )
 )
 

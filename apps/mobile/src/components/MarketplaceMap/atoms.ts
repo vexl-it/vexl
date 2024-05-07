3bbd63966abcdb6695d8937f3283944fa22b998e
Samuel Tremko <samuel@usertechnologies.com>
2024-05-07 09:39:32 +0200

    Fix map points not displayed for offers without listingType

--- apps/mobile/src/components/MarketplaceMap/atoms.ts
+++ apps/mobile/src/components/MarketplaceMap/atoms.ts
@@ -11,8 +11,12 @@ export const mapPointsAtom = atom((get) => {
     .filter((one) =>
       filter.listingType === 'OTHER'
         ? one.offerInfo.publicPart.listingType === filter.listingType
-        : one.offerInfo.publicPart.offerType === filter.offerType &&
+        : filter.listingType === 'PRODUCT'
+        ? one.offerInfo.publicPart.offerType === filter.offerType &&
           one.offerInfo.publicPart.listingType === filter.listingType
+        : (!one.offerInfo.publicPart.listingType ||
+            one.offerInfo.publicPart.listingType === filter.listingType) &&
+          one.offerInfo.publicPart.offerType === filter.offerType
     )
     .flatMap((offer) => {
       const locations = offer.offerInfo.publicPart.location

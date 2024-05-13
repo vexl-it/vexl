b1095f223964f15619c471c234714735abf0ecf4
Samuel Tremko <samuel@usertechnologies.com>
2024-05-13 16:16:06 +0200

    release/1.16.0 (185)

--- apps/mobile/app.config.ts
+++ apps/mobile/app.config.ts
@@ -1,6 +1,6 @@
 // import {SemverString} from '@vexl-next/domain/src/utility/SmeverString.brand'
 
-const VERSION_CODE = 184
+const VERSION_CODE = 185
 const VERSION = '1.16.0'
 const ENV_PRESET = process.env.ENV_PRESET
 const COMMIT_HASH = process.env.EAS_BUILD_GIT_COMMIT_HASH ?? 'local'

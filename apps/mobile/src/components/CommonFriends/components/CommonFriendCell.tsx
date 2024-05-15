c99eac887fdd0ffa3cffe2b0164b5f85e6ed3851
Samuel Tremko <samuel@usertechnologies.com>
2024-05-15 20:30:23 +0200

    Polish UI of common friends list

--- apps/mobile/src/components/CommonFriends/components/CommonFriendCell.tsx
+++ apps/mobile/src/components/CommonFriends/components/CommonFriendCell.tsx
@@ -1,5 +1,5 @@
 import {type UriString} from '@vexl-next/domain/src/utility/UriString.brand'
-import {Image, Text, XStack} from 'tamagui'
+import {getTokens, Image, Text, XStack} from 'tamagui'
 import SvgImage from '../../Image'
 import picturePlaceholderSvg from '../../images/picturePlaceholderSvg'
 
@@ -16,13 +16,19 @@ function CommonFriendCell({imageUri, name, variant}: Props): JSX.Element {
         <Image br="$2" width={30} height={30} source={{uri: imageUri}} />
       ) : (
         <SvgImage
-          height={30}
-          width={30}
+          height={20}
+          width={20}
           borderRadius={8}
           source={picturePlaceholderSvg}
+          fill={getTokens().color.greyOnWhite.val}
         />
       )}
-      <Text ml="$2" col={variant === 'light' ? '$black' : '$white'} fos={16}>
+      <Text
+        ml="$2"
+        col={variant === 'light' ? '$greyOnWhite' : '$white'}
+        ff="$body500"
+        fos={12}
+      >
         {name}
       </Text>
     </XStack>

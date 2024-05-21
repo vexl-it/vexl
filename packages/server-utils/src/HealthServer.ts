1cd5d5ba04091fab95ba992eceee03d17f355786
David Novák <mail@davenov.com>
2024-05-21 08:28:03 +0200

    debug server

--- packages/server-utils/src/HealthServer.ts
+++ packages/server-utils/src/HealthServer.ts
@@ -9,9 +9,21 @@ export function makeHealthServerLive({
 }: {
   port: number
 }): Layer.Layer<never, Http.error.ServeError, never> {
-  const HealtServerLive = NodeHttpServer.server.layer(() => createServer(), {
-    port,
-  })
+  const HealtServerLive = NodeHttpServer.server.layer(
+    () => {
+      const server = createServer()
+      server.on('error', (e) => {
+        console.error('Health server error', e)
+      })
+      server.on('close', () => {
+        console.log('Health server closed')
+      })
+      return server
+    },
+    {
+      port,
+    }
+  )
 
   const HealthHttpLive = Http.router.empty.pipe(
     Http.router.get(
@@ -21,7 +33,9 @@ export function makeHealthServerLive({
   )
 
   const HealthAppLive = HealthHttpLive.pipe(
-    Http.server.serve(),
+    Http.server.serve((a) =>
+      Http.middleware.logger(a).pipe(Effect.withLogSpan('health server'))
+    ),
     Layer.provide(HealtServerLive),
     Layer.provide(
       Layer.effectDiscard(Effect.log(`Health server running on port ${port}`))

eb593bc910de11230d862f63092454d5d150b80c
David Novák <mail@davenov.com>
2024-05-24 11:51:30 +0200

    feat: return 404 when route not found

--- packages/server-utils/src/handleCommonErrorsRouter.ts
+++ packages/server-utils/src/handleCommonErrorsRouter.ts
@@ -5,6 +5,7 @@ import {
   type InternalServerError,
   type NotFoundError,
 } from '@vexl-next/rest-api/src/Errors'
+import {Effect} from 'effect'
 import {type InvalidSessionError} from './ServerUserSession'
 import {type UrlParamsError} from './schemaUrlQuery'
 
@@ -16,47 +17,48 @@ type ErrorsToCatch =
   | InternalServerError
   | NotFoundError
   | UrlParamsError
+  | Http.error.RouteNotFound
 
-const handleCommonErrorsRouter = <C>(
-  r: Http.router.Router<ErrorsToCatch, C>
-): Http.router.Router<Http.body.BodyError, C> =>
-  Http.router.catchAll(r, (e) => {
-    if (e._tag === 'InvalidSessionError') {
-      return Http.response.json(e, {status: 401})
-    }
+const handleCommonErrorsRouter = Effect.catchAll((e: ErrorsToCatch) => {
+  if (e._tag === 'RouteNotFound') {
+    return Http.response.json({message: 'Not found'}, {status: 404})
+  }
+  if (e._tag === 'InvalidSessionError') {
+    return Http.response.json(e, {status: 401})
+  }
 
-    if (e._tag === 'ParseError') {
-      return Http.response.json({message: e.message}, {status: 400})
-    }
+  if (e._tag === 'ParseError') {
+    return Http.response.json({message: e.message}, {status: 400})
+  }
 
-    if (e._tag === 'RequestError' || e._tag === 'UrlParamsError') {
-      return Http.response.json({message: e.message, status: 400})
-    }
+  if (e._tag === 'RequestError' || e._tag === 'UrlParamsError') {
+    return Http.response.json({message: e.message, status: 400})
+  }
 
-    if (e._tag === 'BodyError') {
-      return Http.response.json(
-        {cause: 'BodyError'},
-        {
-          status: 500,
-        }
-      )
-    }
+  if (e._tag === 'BodyError') {
+    return Http.response.json(
+      {cause: 'BodyError'},
+      {
+        status: 500,
+      }
+    )
+  }
 
-    if (e._tag === 'NotFoundError') {
-      return Http.response.json(
-        {message: 'Not found'},
-        {
-          status: 404,
-        }
-      )
-    }
+  if (e._tag === 'NotFoundError') {
+    return Http.response.json(
+      {message: 'Not found'},
+      {
+        status: 404,
+      }
+    )
+  }
 
-    if (e._tag === 'InternalServerError') {
-      return Http.response.json({cause: e.cause}, {status: 500})
-    }
+  if (e._tag === 'InternalServerError') {
+    return Http.response.json({cause: e.cause}, {status: 500})
+  }
 
-    // Should never happen. Why does typescript require this? Inspect it! e is `never` here :/
-    return Http.response.text('Unknown internal server error', {status: 500})
-  })
+  // Should never happen. Why does typescript require this? Inspect it! e is `never` here :/
+  return Http.response.text('Unknown internal server error', {status: 500})
+})
 
 export default handleCommonErrorsRouter

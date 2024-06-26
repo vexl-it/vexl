9988dc0dd72e8113f6a784dc45caa38d09e29509
Samuel Tremko <samuel@usertechnologies.com>
2024-06-26 12:42:42 +0200

    Detox testing setup

--- apps/mobile/src/components/InsideRouter/components/MarketplaceScreen/components/OffersListStateDisplayerContent.tsx
+++ apps/mobile/src/components/InsideRouter/components/MarketplaceScreen/components/OffersListStateDisplayerContent.tsx
@@ -69,7 +69,7 @@ function OffersListStateDisplayerContent(): JSX.Element {
   }
 
   return (
-    <ContainerWithTopBorderRadius>
+    <ContainerWithTopBorderRadius testID="MarketplaceScreen">
       <Stack space="$4">
         <Stack px="$2">
           <BaseFilterDropdown

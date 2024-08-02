ffc0ea770b5aa06bcaec92aee0889ee319c6f49a
Samuel Tremko <samuel@usertechnologies.com>
2024-08-02 11:19:13 +0200

    Change order of action buttons in settings dialogs

--- apps/mobile/src/components/InsideRouter/components/SettingsScreen/components/ReportIssue.tsx
+++ apps/mobile/src/components/InsideRouter/components/SettingsScreen/components/ReportIssue.tsx
@@ -1,61 +1,64 @@
-import {useAtom} from 'jotai'
-import {Platform} from 'react-native'
+import {pipe} from 'fp-ts/lib/function'
+import * as TE from 'fp-ts/TaskEither'
+import {atom, useSetAtom} from 'jotai'
 import {getTokens, Stack, Text, XStack} from 'tamagui'
-import {version} from '../../../../../utils/environment'
-import {useTranslation} from '../../../../../utils/localization/I18nProvider'
-import openUrl from '../../../../../utils/openUrl'
+import {
+  translationAtom,
+  useTranslation,
+} from '../../../../../utils/localization/I18nProvider'
+import {askAreYouSureActionAtom} from '../../../../AreYouSureDialog'
 import Image from '../../../../Image'
-import {reportIssueDialogVisibleAtom} from '../atoms'
+import {contactSupportActionAtom} from '../atoms'
 import emailIconSvg from '../images/emailIconSvg'
-import SettingsScreenDialog from './SettingsScreenDialog'
+
+// atom needs to be defined here to avoid circular dependencies with MainSectionComponent: ReportIssue used in askAreYouSureActionAtom
+export const reportIssueDialogAtom = atom(null, (get, set) => {
+  const {t} = get(translationAtom)
+
+  return pipe(
+    set(askAreYouSureActionAtom, {
+      variant: 'info',
+      steps: [
+        {
+          type: 'StepWithChildren',
+          MainSectionComponent: ReportIssue,
+          positiveButtonText: t('reportIssue.openInEmail'),
+          negativeButtonText: t('common.gotIt'),
+        },
+      ],
+    }),
+    TE.match(
+      () => {},
+      () => {
+        set(contactSupportActionAtom)
+      }
+    )
+  )()
+})
 
 function ReportIssue(): JSX.Element {
   const {t} = useTranslation()
   const tokens = getTokens()
-  const [reportIssueVisible, setReportIssueVisible] = useAtom(
-    reportIssueDialogVisibleAtom
-  )
 
   const supportEmail = t('settings.items.supportEmail')
-  const emailBody = encodeURIComponent(
-    `${t('reportIssue.predefinedBody')}\n\n${Platform.OS}-${version}-${
-      Platform.Version
-    }\n\n`
-  )
+  const contactSupport = useSetAtom(contactSupportActionAtom)
 
   return (
-    <SettingsScreenDialog
-      secondaryButton={{
-        text: t('common.gotIt'),
-      }}
-      primaryButton={{
-        text: t('reportIssue.openInEmail'),
-        onPress: () => {
-          openUrl(
-            `mailto:${supportEmail}?body=${emailBody}`,
-            t('settings.items.supportEmail')
-          )()
-          return true
-        },
-      }}
-      onClose={() => {
-        setReportIssueVisible(false)
-      }}
-      title={t('reportIssue.somethingWentWrong')}
-      subtitle={t('reportIssue.feelFreeToGetInTouch')}
-      visible={reportIssueVisible}
-    >
-      <XStack
-        ai="center"
-        gap="$3"
-        mt="$6"
-        onPress={() => {
-          openUrl(
-            `mailto:${supportEmail}?body=${emailBody}`,
-            t('settings.items.supportEmail')
-          )()
-        }}
+    <Stack space="$2" jc="flex-end">
+      <Text
+        col="$black"
+        my="$4"
+        ff="$heading"
+        fos={28}
+        numberOfLines={2}
+        adjustsFontSizeToFit
       >
+        {t('reportIssue.somethingWentWrong')}
+      </Text>
+      <Text fos={18} ff="$body500" col="$greyOnWhite">
+        {t('reportIssue.feelFreeToGetInTouch')}
+      </Text>
+      <XStack ai="center" gap="$3" mt="$6" onPress={contactSupport}>
         <Stack ai="center" jc="center" bc="$greyAccent5" p="$3" br="$5">
           <Image stroke={tokens.color.greyOnWhite.val} source={emailIconSvg} />
         </Stack>
@@ -63,7 +66,7 @@ function ReportIssue(): JSX.Element {
           {supportEmail}
         </Text>
       </XStack>
-    </SettingsScreenDialog>
+    </Stack>
   )
 }
 

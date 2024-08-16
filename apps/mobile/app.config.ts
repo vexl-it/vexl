dbc317ce3a0c0c1851fd693244a547c80ebc9cce
Samuel Tremko <samuel@usertechnologies.com>
2024-08-16 21:59:21 +0200

    chore: release 1.20.0 (251)

--- apps/mobile/app.config.ts
+++ apps/mobile/app.config.ts
@@ -1,6 +1,6 @@
 // import {SemverString} from '@vexl-next/domain/src/utility/SmeverString.brand'
 
-const VERSION_CODE = 250
+const VERSION_CODE = 251
 const VERSION = '1.20.0'
 const ENV_PRESET = process.env.ENV_PRESET
 const COMMIT_HASH = process.env.EAS_BUILD_GIT_COMMIT_HASH ?? 'local'

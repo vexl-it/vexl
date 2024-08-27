85a5daf0ced5fddd7d183df3a1f832a7bb49f723
David Novák <mail@davenov.com>
2024-08-27 13:53:03 +0200

    fix typecheck error

--- apps/mobile/src/utils/environment.ts
+++ apps/mobile/src/utils/environment.ts
@@ -1,20 +1,20 @@
-import {Schema} from '@effect/schema'
 import {SemverString} from '@vexl-next/domain/src/utility/SmeverString.brand'
-import {VersionCode} from '@vexl-next/domain/src/utility/VersionCode.brand'
+import {type VersionCode} from '@vexl-next/domain/src/utility/VersionCode.brand'
 import Constants from 'expo-constants'
-
-// export const enableHiddenFeatures =
-//   Constants.expoConfig?.extra?.enableHiddenFeatures === true
 
 export const enableHiddenFeatures =
   Constants.expoConfig?.extra?.enableHiddenFeatures
+
 export const apiPreset = Constants.expoConfig?.extra?.apiPreset ?? 'stageEnv'
 export const version = SemverString.parse(
   Constants.expoConfig?.extra?.semver ?? '0.0.0'
 )
-export const versionCode = Schema.decodeSync(VersionCode)(
-  Number(Constants.expoConfig?.extra?.versionCode ?? 0)
-)
+
+// NEver import @effect/schema here or it will break typecheck (Reason is unknown but you are welcome to try)
+export const versionCode = Number(
+  Constants.expoConfig?.extra?.versionCode ?? 0
+) as VersionCode
+
 export const hmacPassword = String(
   Constants.expoConfig?.extra?.hmacPassword ??
     'UHQyykWs4nE1Yn8IQi/lsz2QemK3zA+JIWdGll3PEtle9/aMMBvQk6kKgYkjyewTiK0ypuquBSBVJwuSiYs8FQ=='

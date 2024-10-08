2e28092f87b8f82ad5564dd794962b3fb0446dfe
David Novák <mail@davenov.com>
2024-10-08 14:42:23 +0200

    fix: user-contact table should have unique hash_from,hash_to

--- apps/contact-service/src/db/layer.ts
+++ apps/contact-service/src/db/layer.ts
@@ -6,6 +6,7 @@ import initialMigraiton from './migrations/0001_initial'
 import remove_next_prefix_and_old_users_and_connections from './migrations/0002_remove_next_prefix_and_old_users_and_connections'
 import make_version_in_users_table_nullable from './migrations/0003_make_version_in_users_table_nullable'
 import add_initial_import_done_column_to_users_table from './migrations/0004_add_initial_import_done_column_to_users_table'
+import add_unique_constraint_to_user_contact from './migrations/0004_add_unique_constraint_to_user_contact'
 
 const migrations = [
   {
@@ -27,6 +28,11 @@ const migrations = [
     id: 4,
     name: 'add_initial_import_done_column_to_users_table',
     migrationEffect: add_initial_import_done_column_to_users_table,
+  },
+  {
+    id: 5,
+    name: 'add_unique_constraint_to_user_contact',
+    migrationEffect: add_unique_constraint_to_user_contact,
   },
 ] as const
 

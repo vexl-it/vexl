dcd011e0c64e1e48bd79fb2f08ae2ec3d7f9cb7c
Samuel Tremko <samuel@usertechnologies.com>
2024-11-15 18:03:03 +0100

    feat #1375: Offer detail layout automatic scroll

--- apps/mobile/src/components/OfferDetailScreen/index.tsx
+++ apps/mobile/src/components/OfferDetailScreen/index.tsx
@@ -1,14 +1,18 @@
-import {LinearGradient} from 'expo-linear-gradient'
 import {isSome} from 'fp-ts/Option'
-import {useSetAtom} from 'jotai'
+import {atom, useAtom, useSetAtom} from 'jotai'
 import React, {useEffect} from 'react'
-import {Platform} from 'react-native'
+import {Platform, StyleSheet, type LayoutChangeEvent} from 'react-native'
 import Animated, {
   Extrapolation,
   interpolate,
+  runOnJS,
+  scrollTo,
+  useAnimatedRef,
   useAnimatedScrollHandler,
   useAnimatedStyle,
+  useDerivedValue,
   useSharedValue,
+  type AnimatedRef,
 } from 'react-native-reanimated'
 import {useSafeAreaInsets} from 'react-native-safe-area-context'
 import {Stack, Text, YStack, getTokens} from 'tamagui'
@@ -16,6 +20,7 @@ import {type RootStackScreenProps} from '../../navigationTypes'
 import {useSingleOffer} from '../../state/marketplace'
 import {focusOfferActionAtom} from '../../state/marketplace/atoms/map/focusedOffer'
 import {useTranslation} from '../../utils/localization/I18nProvider'
+import useIsKeyboardShown from '../../utils/useIsKeyboardShown'
 import useSafeGoBack from '../../utils/useSafeGoBack'
 import Button from '../Button'
 import {MAP_SIZE} from '../MarketplaceMap'
@@ -26,53 +31,90 @@ import Title from './components/Title'
 
 type Props = RootStackScreenProps<'OfferDetail'>
 
+// this atom allows user to scroll back down while keyboard is shown
+const userScrolledManuallyAtom = atom(false)
+
+const HEADER_HEIGHT = 50
+
+const styles = StyleSheet.create({
+  mapContainer: {
+    top: 0,
+    left: 0,
+    right: 0,
+    position: 'absolute',
+    zIndex: getTokens().zIndex[1].val,
+  },
+  titleContainer: {
+    top: MAP_SIZE,
+    left: 0,
+    right: 0,
+    position: 'absolute',
+    zIndex: getTokens().zIndex[10].val,
+  },
+})
+
 function OfferDetailScreen({
   route: {
     params: {offerId},
   },
   navigation,
 }: Props): JSX.Element {
+  const animatedRef: AnimatedRef<Animated.ScrollView> = useAnimatedRef()
   const safeGoBack = useSafeGoBack()
   const setFocusedOffer = useSetAtom(focusOfferActionAtom)
   const {t} = useTranslation()
-  const {top} = useSafeAreaInsets()
+  const insets = useSafeAreaInsets()
+  const isKeyboardShown = useIsKeyboardShown()
+  const [userScrolledManually, setUserScrolledManually] = useAtom(
+    userScrolledManuallyAtom
+  )
 
   const offer = useSingleOffer(offerId)
 
   const scrollY = useSharedValue(0)
+  const layoutY = useSharedValue(0)
   const handleScroll = useAnimatedScrollHandler((event) => {
     scrollY.value = event.contentOffset.y
+    runOnJS(setUserScrolledManually)(isKeyboardShown)
   })
 
-  const scrollAnimatedStyles = useAnimatedStyle(() => {
+  const stickyHeader = useAnimatedStyle(() => {
     const translateY = interpolate(
       scrollY.value,
-      [0, 260],
-      [0, -MAP_SIZE],
-      Extrapolation.CLAMP
+      [
+        layoutY.value - insets.top - 1,
+        layoutY.value - insets.top,
+        layoutY.value - insets.top + 1,
+      ],
+      [0, 0, 1]
     )
 
     return {transform: [{translateY}]}
   })
 
-  const safeTopAnimatedHeight = useAnimatedStyle(() => {
-    const height = interpolate(
+  const mapOpacityAnimation = useAnimatedStyle(() => {
+    const translateY = interpolate(scrollY.value, [0, MAP_SIZE], [0, -MAP_SIZE])
+    const opacity = interpolate(
       scrollY.value,
-      [0, 180],
-      [0, top],
+      [0, MAP_SIZE - (HEADER_HEIGHT + insets.top) / 0.9],
+      [1, 0],
       Extrapolation.CLAMP
     )
 
-    return {height}
+    return {transform: [{translateY}], opacity}
   })
 
-  const animatedMapStyles = useAnimatedStyle(() => {
-    const scale = interpolate(scrollY.value, [0, 320], [1.0, 1.3], {
-      extrapolateRight: Extrapolation.CLAMP,
-      extrapolateLeft: Extrapolation.CLAMP,
-    })
-    return {transform: [{scale}]}
+  useDerivedValue(() => {
+    if (isKeyboardShown && !userScrolledManually && Platform.OS === 'ios')
+      // Infinity is used to scroll to the bottom of the scroll view
+      scrollTo(animatedRef, 0, Infinity, true)
   })
+
+  useEffect(() => {
+    if (!isKeyboardShown) {
+      setUserScrolledManually(false)
+    }
+  }, [isKeyboardShown, setUserScrolledManually])
 
   useEffect(() => {
     setFocusedOffer(offerId)
@@ -93,49 +135,54 @@ function OfferDetailScreen({
           offer.value.offerInfo.publicPart.locationState.includes(
             'IN_PERSON'
           ) && (
-            <Animated.View style={animatedMapStyles}>
+            <Animated.View style={[mapOpacityAnimation, styles.mapContainer]}>
               <MarketplaceMapContainer />
             </Animated.View>
           )}
         {isSome(offer) ? (
-          <>
+          <Stack f={1}>
+            <Stack
+              position="absolute"
+              top={0}
+              left={0}
+              right={0}
+              height={insets.top}
+              backgroundColor="black"
+              zIndex="$10"
+            />
             {offer.value.offerInfo.publicPart.location.length > 0 ? (
-              <Animated.View style={scrollAnimatedStyles}>
-                <Stack mah={top}>
-                  <Animated.View style={safeTopAnimatedHeight}>
-                    <LinearGradient
-                      colors={[
-                        'transparent',
-                        'transparent',
-                        getTokens().color.black.val,
-                      ]}
-                      style={{flex: 1}}
-                    />
-                  </Animated.View>
-                </Stack>
-                <Title offer={offer.value} />
-                <Animated.ScrollView
-                  overScrollMode="always"
-                  contentContainerStyle={{
-                    paddingBottom: Platform.OS === 'ios' ? MAP_SIZE : 0,
+              <Animated.ScrollView
+                ref={animatedRef}
+                overScrollMode="always"
+                scrollEventThrottle={16}
+                contentContainerStyle={{
+                  paddingTop: MAP_SIZE + HEADER_HEIGHT,
+                }}
+                style={{
+                  backgroundColor: getTokens().color.black.val,
+                }}
+                onScroll={handleScroll}
+                showsVerticalScrollIndicator={false}
+              >
+                <Animated.View
+                  onLayout={(event: LayoutChangeEvent) => {
+                    'worklet'
+                    layoutY.value = event.nativeEvent.layout.y
                   }}
-                  style={{
-                    backgroundColor: getTokens().color.black.val,
-                  }}
-                  onScroll={handleScroll}
-                  showsVerticalScrollIndicator={false}
+                  style={[stickyHeader, styles.titleContainer]}
                 >
-                  <OfferInfo
-                    mapIsVisible
-                    navigation={navigation}
-                    offer={offer.value}
-                  />
-                </Animated.ScrollView>
-              </Animated.View>
+                  <Title offer={offer.value} />
+                </Animated.View>
+                <OfferInfo
+                  mapIsVisible
+                  navigation={navigation}
+                  offer={offer.value}
+                />
+              </Animated.ScrollView>
             ) : (
               <OfferInfo navigation={navigation} offer={offer.value} />
             )}
-          </>
+          </Stack>
         ) : (
           <YStack
             f={1}

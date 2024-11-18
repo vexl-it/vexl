e7bb438ea4b8d5b5a56c39ca02e6ee709e1870e7
David Novák <mail@davenov.com>
2024-11-18 10:11:20 +0100

    feat: set aps environment based on environment

--- apps/mobile/app.config.ts
+++ apps/mobile/app.config.ts
@@ -8,6 +8,8 @@ const COMMIT_HASH = process.env.EAS_BUILD_GIT_COMMIT_HASH ?? 'local'
 // // check if version is valid
 // SemverString.parse(VERSION)
 
+console.log('Building the app with preset:', process.env.NODE_ENV)
+
 const presets = {
   stage: {
     enableHiddenFeatures: true,
@@ -98,7 +100,8 @@ export default {
       ],
     },
     'entitlements': {
-      'aps-environment': 'development',
+      'aps-environment':
+        process.env.NODE_ENV === 'development' ? 'development' : 'production',
     },
   },
   'android': {

5527111e61659ed9d94a965029ef22b6bdfb2210
David Novák <mail@davenov.com>
2024-11-25 09:47:21 +0100

    add link route to account deletion website

--- apps/account-deletion-website/app/entry.server.tsx
+++ apps/account-deletion-website/app/entry.server.tsx
@@ -4,7 +4,7 @@ import {PassThrough} from 'node:stream'
 import type {AppLoadContext, EntryContext} from '@remix-run/node'
 import {createReadableStreamFromReadable} from '@remix-run/node'
 import {RemixServer} from '@remix-run/react'
-import isbotModule from 'isbot'
+import {isbot as isbotModule} from 'isbot'
 import {renderToPipeableStream} from 'react-dom/server'
 
 const ABORT_DELAY = 5_000

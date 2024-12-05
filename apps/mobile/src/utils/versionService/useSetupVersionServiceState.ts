44056d30b7b175957221a3f0129f167dfd2c426d
David Novák <mail@davenov.com>
2024-12-05 13:53:08 +0100

    release: 1.23.4 (344)

--- apps/mobile/src/utils/versionService/useSetupVersionServiceState.ts
+++ apps/mobile/src/utils/versionService/useSetupVersionServiceState.ts
@@ -3,37 +3,41 @@ import {useAtomValue, useSetAtom} from 'jotai'
 import {useCallback} from 'react'
 import {apiAtom} from '../../api'
 import reportError from '../reportError'
-import {useOnFocusAndAppState} from '../useFocusAndAppState'
+import {useAppState} from '../useAppState'
 import {versionServiceAtom} from './atoms'
 
 export default function useSetupVersionServiceState(): void {
   const api = useAtomValue(apiAtom)
   const setVersionServiceState = useSetAtom(versionServiceAtom)
-  useOnFocusAndAppState(
-    useCallback(() => {
-      console.log('Fetching version service info')
-      void api.user.getVersionServiceInfo().pipe(
-        Effect.andThen((info) => {
-          setVersionServiceState({
-            offerRerequestLimitDays: info.offerRerequestLimitDays,
-            requestForceUpdate: info.requestForceUpdate,
-            maintenanceUntil: info.maintenanceUntil.pipe(
-              Option.getOrElse(() => undefined)
-            ),
-          })
-          console.log('Version service info fetched', JSON.stringify(info))
-        }),
-        Effect.mapError((e) => {
-          console.log('Error while fetching version service info')
-          reportError(
-            'warn',
-            new Error('Error while fetching version service info'),
-            {e}
-          )
-          return Effect.void
-        }),
-        Effect.runPromise
-      )
-    }, [api, setVersionServiceState])
+  useAppState(
+    useCallback(
+      (appState) => {
+        if (appState !== 'active') return
+        console.log('Fetching version service info')
+        void api.user.getVersionServiceInfo().pipe(
+          Effect.andThen((info) => {
+            setVersionServiceState({
+              offerRerequestLimitDays: info.offerRerequestLimitDays,
+              requestForceUpdate: info.requestForceUpdate,
+              maintenanceUntil: info.maintenanceUntil.pipe(
+                Option.getOrElse(() => undefined)
+              ),
+            })
+            console.log('Version service info fetched', JSON.stringify(info))
+          }),
+          Effect.mapError((e) => {
+            console.log('Error while fetching version service info')
+            reportError(
+              'warn',
+              new Error('Error while fetching version service info'),
+              {e}
+            )
+            return Effect.void
+          }),
+          Effect.runPromise
+        )
+      },
+      [api, setVersionServiceState]
+    )
   )
 }

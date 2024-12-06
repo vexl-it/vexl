2192ee4845a3f1c541245448a156dbbfcbc07e13
Samuel Tremko <samuel@usertechnologies.com>
2024-12-06 19:27:59 +0100

    TradeChecklist message history

--- apps/mobile/src/state/tradeChecklist/utils/amount.ts
+++ apps/mobile/src/state/tradeChecklist/utils/amount.ts
@@ -1,4 +1,4 @@
-import {type AmountData} from '@vexl-next/domain/src/general/tradeChecklist'
+import {type AmountChatMessage} from '@vexl-next/domain/src/general/tradeChecklist'
 import {UnixMilliseconds0} from '@vexl-next/domain/src/utility/UnixMilliseconds.brand'
 import fastDeepEqual from 'fast-deep-equal'
 import calculatePercentageDifference from '../../../utils/calculatePercentageDifference'
@@ -10,11 +10,11 @@ type AmountInState = TradeChecklistInState['amount']
 
 interface AmountDataToDisplay {
   by: 'me' | 'them'
-  amountData: AmountData
+  amountData: AmountChatMessage
   status: 'accepted' | 'pending'
 }
 
-export function getAmountData(
+export function getLatestAmountDataMessage(
   data: AmountInState
 ): AmountDataToDisplay | undefined {
   const sentAmountData = data.sent
@@ -73,11 +73,11 @@ export function calculateBtcPricePercentageDifference(
 }
 
 export function amountSettled(data: AmountInState): boolean {
-  return getAmountData(data)?.status === 'accepted'
+  return getLatestAmountDataMessage(data)?.status === 'accepted'
 }
 
 export function amountPending(data: AmountInState): boolean {
-  return getAmountData(data)?.status === 'pending'
+  return getLatestAmountDataMessage(data)?.status === 'pending'
 }
 
 export function applyFeeOnNumberValue(

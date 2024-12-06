713e877adffb7f7241e079cad79a724dfe9e3237
David Novák <mail@davenov.com>
2024-12-06 11:39:02 +0100

    feat: remove patch and combine crypto with fallback using metro

--- apps/mobile/babel.config.js
+++ apps/mobile/babel.config.js
@@ -5,21 +5,21 @@ module.exports = function (api) {
   return {
     presets: ['babel-preset-expo'],
     plugins: [
-      [
-        'module-resolver',
-        {
-          alias: {
-            'crypto': 'react-native-quick-crypto',
-            'node:crypto': 'react-native-quick-crypto',
-            // 'crypto': 'crypto-browserify',
-            // 'node:crypto': 'crypto-browserify',
-            'stream': 'stream-browserify',
-            'buffer': '@craftzdog/react-native-buffer',
-            'node:buffer': '@craftzdog/react-native-buffer',
-            'brorand': '@vexl-next/fix-brorand-for-expo',
-          },
-        },
-      ],
+      // [
+      //   'module-resolver',
+      //   {
+      //     alias: {
+      //       'crypto': 'react-native-quick-crypto',
+      //       'node:crypto': 'react-native-quick-crypto',
+      //       // 'crypto': 'crypto-browserify',
+      //       // 'node:crypto': 'crypto-browserify',
+      //       'stream': 'stream-browserify',
+      //       'buffer': '@craftzdog/react-native-buffer',
+      //       'node:buffer': '@craftzdog/react-native-buffer',
+      //       'brorand': '@vexl-next/fix-brorand-for-expo',
+      //     },
+      //   },
+      // ],
       [
         '@tamagui/babel-plugin',
         {
--- apps/mobile/metro.config.js
+++ apps/mobile/metro.config.js
@@ -19,4 +19,37 @@ config.resolver.nodeModulesPaths = [
 // 3. Force Metro to resolve (sub)dependencies only from the `nodeModulesPaths`
 config.resolver.disableHierarchicalLookup = true
 
+config.resolver.resolveRequest = (context, moduleName, platform) => {
+  if (moduleName === 'crypto' || moduleName === 'node:crypto') {
+    // return require.resolve(path.resolve(projectRoot, 'merged-crypto'))
+
+    return {
+      filePath: path.resolve(projectRoot, 'merged-crypto.js'),
+      type: 'sourceFile',
+    }
+    // return require.resolve(path.resolve(projectRoot, 'merged-crypto'))
+  }
+
+  if (moduleName === 'stream') {
+    return context.resolveRequest(context, 'stream-browserify', platform)
+  }
+
+  // if (moduleName === 'buffer' || moduleName === 'node:buffer') {
+  //   return context.resolveRequest(
+  //     context,
+  //     '@craftzdog/react-native-buffer',
+  //     platform
+  //   )
+  // }
+
+  // if (moduleName === 'brorand') {
+  //   return context.resolveRequest(
+  //     context,
+  //     '@vexl-next/fix-brorand-for-expo',
+  //     platform
+  //   )
+  // }
+  return context.resolveRequest(context, moduleName, platform)
+}
+
 module.exports = config

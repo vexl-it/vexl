4eb3e655199dee5d756f82aa49edf73a4815c830
David Novák <mail@davenov.com>
2025-01-13 14:47:02 +0100

    feat: report messages pulled

--- apps/chat-service/src/db/MessagesDbService/query/createDeletePulledMessagesByInboxId.ts
+++ apps/chat-service/src/db/MessagesDbService/query/createDeletePulledMessagesByInboxId.ts
@@ -1,25 +1,37 @@
 import {SqlSchema} from '@effect/sql'
 import {PgClient} from '@effect/sql-pg'
 import {UnexpectedServerError} from '@vexl-next/domain/src/general/commonErrors'
-import {Effect, flow} from 'effect'
+import {Effect, flow, Option, Schema} from 'effect'
 import {InboxRecordId} from '../../InboxDbService/domain'
 
 export const createDeletePulledMessagesMessagesByInboxId = Effect.gen(
   function* (_) {
     const sql = yield* _(PgClient.PgClient)
 
-    const query = SqlSchema.void({
+    const query = SqlSchema.findOne({
+      Result: Schema.Struct({count: Schema.NumberFromString}),
       Request: InboxRecordId,
       execute: (params) => sql`
-        DELETE FROM message
-        WHERE
-          inbox_id = ${params}
-          AND pulled = TRUE
+        WITH
+          deleted AS (
+            DELETE FROM message
+            WHERE
+              inbox_id = ${params}
+              AND pulled = TRUE
+          )
+        SELECT
+          count(*) AS COUNT
       `,
     })
 
     return flow(
       query,
+      Effect.map(
+        flow(
+          Option.map((r) => r.count),
+          Option.getOrElse(() => 0)
+        )
+      ),
       Effect.catchAll((e) =>
         Effect.zipRight(
           Effect.logError('Error in deletePulledMessagesByInboxId', e),

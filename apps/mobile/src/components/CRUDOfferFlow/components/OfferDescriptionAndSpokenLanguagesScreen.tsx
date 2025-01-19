b2e5418a6c457f7b7e838da412b5208493c688b6
Samuel Tremko <samuel@usertechnologies.com>
2025-01-19 17:39:54 +0100

    Fix: Scroll to end on description input focus in offer creation on iOS

--- apps/mobile/src/components/CRUDOfferFlow/components/OfferDescriptionAndSpokenLanguagesScreen.tsx
+++ apps/mobile/src/components/CRUDOfferFlow/components/OfferDescriptionAndSpokenLanguagesScreen.tsx
@@ -1,16 +1,25 @@
 import {useMolecule} from 'bunshi/dist/react'
+import {Platform} from 'react-native'
+import Animated, {
+  scrollTo,
+  useAnimatedRef,
+  useDerivedValue,
+  type AnimatedRef,
+} from 'react-native-reanimated'
 import {getTokens} from 'tamagui'
 import {useTranslation} from '../../../utils/localization/I18nProvider'
+import useIsKeyboardShown from '../../../utils/useIsKeyboardShown'
 import Description from '../../OfferForm/components/Description'
 import SpokenLanguages from '../../OfferForm/components/SpokenLanguages'
 import Section from '../../Section'
 import spokenLanguagesSvg from '../../images/spokenLanguagesSvg'
 import {offerFormMolecule} from '../atoms/offerFormStateAtoms'
 import descriptionSvg from '../images/descriptionSvg'
-import ScreenWrapper from './ScreenWrapper'
 
 function OfferDescriptionAndSpokenLanguagesScreen(): JSX.Element {
   const {t} = useTranslation()
+  const animatedRef: AnimatedRef<Animated.ScrollView> = useAnimatedRef()
+  const isKeyboardShown = useIsKeyboardShown()
   const {
     offerDescriptionAtom,
     listingTypeAtom,
@@ -22,8 +31,18 @@ function OfferDescriptionAndSpokenLanguagesScreen(): JSX.Element {
     saveSelectedSpokenLanguagesActionAtom,
   } = useMolecule(offerFormMolecule)
 
+  useDerivedValue(() => {
+    if (isKeyboardShown && Platform.OS === 'ios')
+      // Infinity is used to scroll to the bottom of the scroll view
+      scrollTo(animatedRef, 0, Infinity, true)
+  })
+
   return (
-    <ScreenWrapper>
+    <Animated.ScrollView
+      style={{flex: 1, backgroundColor: getTokens().color.black.val}}
+      ref={animatedRef}
+      showsVerticalScrollIndicator={false}
+    >
       <Section
         title={t('offerForm.spokenLanguages.language')}
         image={spokenLanguagesSvg}
@@ -51,7 +70,7 @@ function OfferDescriptionAndSpokenLanguagesScreen(): JSX.Element {
           offerTypeAtom={offerTypeAtom}
         />
       </Section>
-    </ScreenWrapper>
+    </Animated.ScrollView>
   )
 }
 

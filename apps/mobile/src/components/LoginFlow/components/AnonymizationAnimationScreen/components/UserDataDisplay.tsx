83276c738b2f0adedecb929f85226e4b2301628b
David Novák <mail@davenov.com>
2025-04-07 16:19:40 +0200

    fix: remove unnecessary log

--- apps/mobile/src/components/LoginFlow/components/AnonymizationAnimationScreen/components/UserDataDisplay.tsx
+++ apps/mobile/src/components/LoginFlow/components/AnonymizationAnimationScreen/components/UserDataDisplay.tsx
@@ -11,8 +11,6 @@ interface Props {
 }
 
 function UserDataDisplay({topText, realLifeInfo, style}: Props): JSX.Element {
-  console.log(JSON.stringify(realLifeInfo?.image))
-  console.log('wtf')
   return (
     <Stack ai="center" jc="center" style={style}>
       {!!topText && (

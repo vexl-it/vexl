10ab406fdf569a1d062634e0e47466b6c10674ee
Samuel Tremko <samuel@usertechnologies.com>
2025-04-22 13:15:14 +0200

    feat #1659: Add vexlak to empty clubs screen

--- apps/mobile/src/components/EventsAndClubsScreen/components/ClubsScreen/components/EmptyListPlaceholder.tsx
+++ apps/mobile/src/components/EventsAndClubsScreen/components/ClubsScreen/components/EmptyListPlaceholder.tsx
@@ -1,13 +1,28 @@
+import {useNavigation} from '@react-navigation/native'
 import {Text, YStack} from 'tamagui'
 import {useTranslation} from '../../../../../utils/localization/I18nProvider'
+import Button from '../../../../Button'
+import {ImageUniversal} from '../../../../Image'
+import anonymousAvatarSvg from '../../../../images/anonymousAvatarSvg'
 
 export function EmptyListPlaceholder(): JSX.Element {
   const {t} = useTranslation()
+  const navigation = useNavigation()
+
   return (
-    <YStack f={1} alignItems="center" alignContent="center">
-      <Text ff="$body600" fs={24}>
+    <YStack f={1} alignItems="center" justifyContent="center" gap="$4">
+      <ImageUniversal source={{type: 'svgXml', svgXml: anonymousAvatarSvg}} />
+      <Text textAlign="center" col="$greyOnWhite" fos={20}>
         {t('clubs.noClubYet')}
       </Text>
+      <Button
+        size="large"
+        variant="secondary"
+        text={t('suggestion.whatAreClubs')}
+        onPress={() => {
+          navigation.navigate('Faqs', {pageType: 'WHAT_ARE_VEXL_CLUBS'})
+        }}
+      />
     </YStack>
   )
 }

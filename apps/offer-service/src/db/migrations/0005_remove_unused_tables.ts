4b8386346bd51f5c61ba5bb759a4c4acff4af425
David Novák <mail@davenov.com>
2025-05-30 13:52:47 +0200

    feat: #1365 - remove unused tables

---
+++ apps/offer-service/src/db/migrations/0005_remove_unused_tables.ts
@@ -0,0 +1,13 @@
+import {SqlClient} from '@effect/sql'
+import {Effect} from 'effect'
+
+export default Effect.flatMap(
+  SqlClient.SqlClient,
+  (sql) => sql`
+    DROP TABLE IF EXISTS "databasechangelog";
+
+    DROP TABLE IF EXISTS "databasechangeloglock";
+
+    DROP TABLE IF EXISTS "stats";
+  `
+)

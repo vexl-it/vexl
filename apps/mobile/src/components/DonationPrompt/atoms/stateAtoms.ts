6c09a18a59fad183d649b384506a54741f800b70
Samuel Tremko <samuel@usertechnologies.com>
2025-06-27 15:07:58 +0200

    feat #1807: Add predefined values into donation prompt

--- apps/mobile/src/components/DonationPrompt/atoms/stateAtoms.ts
+++ apps/mobile/src/components/DonationPrompt/atoms/stateAtoms.ts
@@ -2,11 +2,21 @@ import {atom} from 'jotai'
 import {DateTime} from 'luxon'
 import {lastDisplayOfDonationPromptTimestampAtom} from '../../../utils/preferences'
 
+export const PREDEFINED_DONATION_AMOUNTS = [10, 20, 30, 50]
 export const MAX_DONATION_AMOUNT = 1000 // Maximum donation amount in EUR
 export const DONATION_PROMPT_DAYS_THRESHOLD_COUNT = 2
 
 export const donationAmountAtom = atom<string | undefined>(undefined)
+export const selectedPredefinedDonationValueAtom = atom<number | undefined>(
+  undefined
+)
 export const donationPaymentMethodAtom = atom<'BTC-CHAIN' | 'BTC-LN'>('BTC-LN')
+
+export const resetDonationPromptValuesActionAtom = atom(null, (get, set) => {
+  set(donationAmountAtom, undefined)
+  set(selectedPredefinedDonationValueAtom, undefined)
+  set(donationPaymentMethodAtom, 'BTC-LN')
+})
 
 export const shouldShowDonationPromptAtom = atom((get) => {
   const lastDisplayOfDonationPromptTimestamp = get(
---
+++ apps/mobile/src/components/DonationPrompt/components/PredefinedDonationValue.tsx
@@ -0,0 +1,40 @@
+import {TouchableOpacity, type TouchableOpacityProps} from 'react-native'
+import {Stack, Text, XStack} from 'tamagui'
+import Image from '../../Image'
+import checkmarkInCircleSvg from '../../images/checkmarkInCircleSvg'
+
+interface Props extends TouchableOpacityProps {
+  selected: boolean
+  title: string
+}
+
+function PredefinedDonationValue({
+  selected,
+  title,
+  ...rest
+}: Props): JSX.Element {
+  return (
+    <TouchableOpacity {...rest}>
+      <XStack gap="$2">
+        <Stack
+          width={22}
+          height={22}
+          ai="center"
+          jc="center"
+          bw={1}
+          borderColor="$grey"
+          br={20}
+        >
+          {!!selected && (
+            <Image width={20} height={20} source={checkmarkInCircleSvg} />
+          )}
+        </Stack>
+        <Text ff="$body600" fos={18} col="$grey">
+          {title}
+        </Text>
+      </XStack>
+    </TouchableOpacity>
+  )
+}
+
+export default PredefinedDonationValue

d58cece411d1f53ce1cd2750988abeaecf24283c
Samuel Tremko <samuel@usertechnologies.com>
2025-07-04 11:41:52 +0200

    feat #1808: Add approx value in SATS for donation prompt

---
+++ apps/mobile/src/components/DonationPrompt/components/DonationPriceInSats.tsx
@@ -0,0 +1,71 @@
+import {useAtomValue, useSetAtom} from 'jotai'
+import {useEffect, useMemo} from 'react'
+import {Text, XStack} from 'tamagui'
+import {
+  createBtcPriceForCurrencyAtom,
+  refreshBtcPriceActionAtom,
+  SATOSHIS_IN_BTC,
+} from '../../../state/currentBtcPriceAtoms'
+import {useTranslation} from '../../../utils/localization/I18nProvider'
+import VexlActivityIndicator from '../../LoadingOverlayProvider/VexlActivityIndicator'
+import {
+  donationAmountAtom,
+  selectedPredefinedDonationValueAtom,
+} from '../atoms/stateAtoms'
+
+function DonationPriceInSats(): JSX.Element {
+  const {t} = useTranslation()
+
+  const donationAmount = useAtomValue(donationAmountAtom)
+  const selectedPredefinedDonationValue = useAtomValue(
+    selectedPredefinedDonationValueAtom
+  )
+  const refreshBtcPrice = useSetAtom(refreshBtcPriceActionAtom)
+  const btcPriceWithState = useAtomValue(
+    useMemo(() => createBtcPriceForCurrencyAtom('EUR'), [])
+  )
+
+  const donationInSats = useMemo(() => {
+    if (donationAmount && btcPriceWithState?.btcPrice) {
+      return Math.round(
+        (Number(donationAmount) / btcPriceWithState.btcPrice.BTC) *
+          SATOSHIS_IN_BTC
+      )
+    }
+
+    if (selectedPredefinedDonationValue && btcPriceWithState?.btcPrice) {
+      return Math.round(
+        (Number(selectedPredefinedDonationValue) /
+          btcPriceWithState.btcPrice.BTC) *
+          SATOSHIS_IN_BTC
+      )
+    }
+
+    return 0
+  }, [
+    btcPriceWithState?.btcPrice,
+    donationAmount,
+    selectedPredefinedDonationValue,
+  ])
+
+  useEffect(() => {
+    void refreshBtcPrice('EUR')()
+  }, [refreshBtcPrice])
+
+  return (
+    <XStack als="flex-start" gap="$1" ai="center" flexWrap="wrap">
+      <Text col="$greyAccent2">
+        {`${t('offer.approximatelyAbbreviation')}`}
+      </Text>
+      {btcPriceWithState?.state === 'loading' ? (
+        <VexlActivityIndicator size="small" bc="$greyAccent2" />
+      ) : (
+        <Text col="$greyAccent2">{donationInSats}</Text>
+      )}
+      <Text col="$greyAccent2">{t('common.SATS')}</Text>
+      <Text col="$greyAccent2">{`(${t('settings.btcPriceSourceCreditYadio')})`}</Text>
+    </XStack>
+  )
+}
+
+export default DonationPriceInSats

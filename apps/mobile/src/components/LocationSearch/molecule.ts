0503b81be10e3d6ea81571712bc2d85b67a435a1
David Novák <mail@davenov.com>
2025-10-23 10:33:15 +0200

    fix: location picker not working

--- apps/mobile/src/components/LocationSearch/molecule.ts
+++ apps/mobile/src/components/LocationSearch/molecule.ts
@@ -1,18 +1,17 @@
 import {type LocationSuggestion} from '@vexl-next/rest-api/src/services/location/contracts'
-import {createScope, molecule, type MoleculeOrInterface} from 'bunshi'
-import {useMolecule} from 'bunshi/dist/react'
-import {Effect, Either, Exit, Fiber} from 'effect'
-import * as E from 'fp-ts/Either'
-import {pipe} from 'fp-ts/lib/function'
+import {
+  createScope,
+  molecule,
+  type MoleculeOrInterface,
+  use,
+  useMolecule,
+} from 'bunshi/dist/react'
+import {randomUUID} from 'crypto'
 import {atom} from 'jotai'
 import {splitAtom} from 'jotai/utils'
-import {randomUUID} from 'node:crypto'
 import {z} from 'zod'
 import {apiAtom} from '../../api'
-import {
-  loadableEffectEither,
-  type UnknownLoadingError,
-} from '../../utils/atomUtils/loadableEither'
+import {createEffectAtomWithProgress} from '../../utils/atomUtils/createEffectAtomWithProgress'
 import {getCurrentLocale} from '../../utils/localization/I18nProvider'
 
 export const LocationSessionId = z.string().uuid().brand<'LocationSessionId'>()
@@ -20,90 +19,36 @@ export type LocationSessionId = z.TypeOf<typeof LocationSessionId>
 export function newLocationSessionId(): LocationSessionId {
   return LocationSessionId.parse(randomUUID())
 }
+export const LocationSearchScope = createScope<LocationSessionId>(
+  newLocationSessionId()
+)
 
-export const LocationSearchScope = createScope<{sessionId: LocationSessionId}>({
-  sessionId: newLocationSessionId(),
-})
+export const LocationSearchMolecule = molecule(() => {
+  use(LocationSearchScope)
+  const searchQueryInnerAtom = atom('')
 
-export const LocationSearchMolecule = molecule((_, getScope) => {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const sessionId = getScope(LocationSearchScope).sessionId // We need to use this to make sure the scope is used
-
-  const searchQueryAtom = atom('')
-
-  const searchResultsAtom = loadableEffectEither(
-    atom(
-      async (
-        get,
-        {signal}
-      ): Promise<
-        Either.Either<
-          readonly LocationSuggestion[],
-          | Effect.Effect.Error<
-              ReturnType<typeof api.location.getLocationSuggestions>
-            >
-          | UnknownLoadingError
-        >
-      > => {
-        const api = get(apiAtom)
-        const query = get(searchQueryAtom)
-        if (query.trim() === '') return Either.right([])
-
-        const fiber = Effect.runFork(
-          api.location
-            .getLocationSuggestions({
-              query: {
-                phrase: query,
-                lang: getCurrentLocale(),
-              },
-            })
-            .pipe(
-              Effect.map((one) => {
-                return one.result
-              }),
-              Effect.either
-            )
-        )
-
-        signal.onabort = () => {
-          Effect.runFork(Fiber.interruptFork(fiber))
-        }
-
-        const exit = await Effect.runPromise(Fiber.await(fiber))
-
-        if (Exit.isSuccess(exit)) {
-          return exit.value
-        }
-
-        return Either.left({
-          _tag: 'UnknownLoadingError',
-          error: exit.cause,
-        } as UnknownLoadingError)
-      }
-    )
-  )
-
-  const searchResultsOrEmptyArrayAtom = atom((get): LocationSuggestion[] => {
-    const searchResults = get(searchResultsAtom)
-    if (searchResults.state === 'loading') return []
-    return pipe(
-      searchResults.either,
-      E.getOrElseW(() => []),
-      (one) => [...one]
-    )
+  const {
+    resultAtom: searchResultsAtom,
+    effectiveInputAtom: searchQueryAtom,
+    isLoadingAtom,
+    errorAtom,
+    successAtom,
+  } = createEffectAtomWithProgress({
+    inputAtom: searchQueryInnerAtom,
+    effectToRun: (query, get) =>
+      get(apiAtom).location.getLocationSuggestions({
+        query: {
+          phrase: query,
+          lang: getCurrentLocale(),
+        },
+      }),
   })
+
+  const searchResultsOrEmptyArrayAtom = atom(
+    // Split atom does not accept readonly -______-
+    (get): LocationSuggestion[] => [...(get(successAtom)?.result ?? [])]
+  )
   const searchResultsAtomsAtom = splitAtom(searchResultsOrEmptyArrayAtom)
-  const isLoadingAtom = atom(
-    (get) => get(searchResultsAtom).state === 'loading'
-  )
-  const errorAtom = atom((get) => {
-    const result = get(searchResultsAtom)
-
-    if (result.state === 'done' && E.isLeft(result.either)) {
-      return result.either.left
-    }
-    return null
-  })
 
   return {
     searchQueryAtom,

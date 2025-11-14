85f0c00948ca6cafd8338dcb4a590d750afe4710
David Novák <mail@davenov.com>
2025-11-14 14:31:25 +0100

    release: 1.37.5 (641)

--- apps/mobile/app.config.ts
+++ apps/mobile/app.config.ts
@@ -1,7 +1,7 @@
 // import {SemverString} from '@vexl-next/domain/src/utility/SmeverString.brand'
 
-const VERSION_CODE = 640
-const VERSION = '1.37.4'
+const VERSION_CODE = 641
+const VERSION = '1.37.5'
 const ENV_PRESET = process.env.ENV_PRESET
 const COMMIT_HASH = process.env.EAS_BUILD_GIT_COMMIT_HASH ?? 'local'
 const APP_SOURCE = process.env.APP_SOURCE ?? 'local'

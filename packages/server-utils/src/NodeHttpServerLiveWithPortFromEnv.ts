6fb9bb2dad9dfba736aaa190722c6d675dff947b
SamTremko <samueltremko@gmail.com>
2025-11-18 13:18:34 +0100

    feat #825: Add minimal timeout to every network request

--- packages/server-utils/src/NodeHttpServerLiveWithPortFromEnv.ts
+++ packages/server-utils/src/NodeHttpServerLiveWithPortFromEnv.ts
@@ -1,9 +1,29 @@
 import {NodeHttpServer} from '@effect/platform-node'
 import {Effect, Layer} from 'effect/index'
 import {createServer} from 'http'
-import {portConfig} from './commonConfigs'
+import {
+  headersTimeoutMsConfig,
+  keepAliveTimeoutMsConfig,
+  portConfig,
+  requestTimeoutMsConfig,
+} from './commonConfigs'
 
-export const NodeHttpServerLiveWithPortFromEnv = portConfig.pipe(
-  Effect.map((port) => NodeHttpServer.layer(createServer, {port})),
-  Layer.unwrapEffect
-)
+export const NodeHttpServerLiveWithPortFromEnv = Effect.gen(function* (_) {
+  const port = yield* _(portConfig)
+  const keepAliveTimeoutMs = yield* _(keepAliveTimeoutMsConfig)
+  const headersTimeoutMs = yield* _(headersTimeoutMsConfig)
+  const requestTimeoutMs = yield* _(requestTimeoutMsConfig)
+
+  return NodeHttpServer.layer(
+    () => {
+      const server = createServer()
+
+      server.keepAliveTimeout = keepAliveTimeoutMs
+      server.headersTimeout = headersTimeoutMs
+      server.requestTimeout = requestTimeoutMs
+
+      return server
+    },
+    {port}
+  )
+}).pipe(Layer.unwrapEffect)

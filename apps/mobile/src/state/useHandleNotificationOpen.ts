f923b0c0e6e327e251caeb2528e4135550320dda
SamTremko <samueltremko@gmail.com>
2025-12-16 14:05:05 +0100

    feat #2109: Open chats list after pressing system notification

--- apps/mobile/src/state/useHandleNotificationOpen.ts
+++ apps/mobile/src/state/useHandleNotificationOpen.ts
@@ -132,8 +132,19 @@ const reactOnNotificationOpenAtom = atom(
     })
 )
 
+const reactOnSystemNotificationOpenAtom = atom(null, (get, set) =>
+  Effect.gen(function* (_) {
+    if (navigationRef.isReady()) {
+      navigationRef.navigate('InsideTabs', {screen: 'Messages'})
+    }
+  })
+)
+
 export default function useHandleNotificationOpen(): void {
   const reactOnNotificationOpen = useSetAtom(reactOnNotificationOpenAtom)
+  const reactOnSystemNotificationOpen = useSetAtom(
+    reactOnSystemNotificationOpenAtom
+  )
 
   useAppState(
     useCallback(
@@ -141,13 +152,16 @@ export default function useHandleNotificationOpen(): void {
         if (appState !== 'active') return
         void (async () => {
           const initialNotification = await notifee.getInitialNotification()
-          if (initialNotification)
+          if (initialNotification) {
             await Effect.runPromise(
               reactOnNotificationOpen(initialNotification.notification)
             )
+          } else {
+            await Effect.runPromise(reactOnSystemNotificationOpen())
+          }
         })()
       },
-      [reactOnNotificationOpen]
+      [reactOnNotificationOpen, reactOnSystemNotificationOpen]
     )
   )
   useEffect(() => {

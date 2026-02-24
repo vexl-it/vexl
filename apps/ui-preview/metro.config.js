1da85869681f0bf61be2d528e0fc521bd9b42cde
SamTremko <samueltremko@gmail.com>
2026-02-24 13:06:36 +0100

    feat #2127: Tamagui configuration and theming

---
+++ apps/ui-preview/metro.config.js
@@ -0,0 +1,19 @@
+// Learn more https://docs.expo.dev/guides/monorepos
+const {getDefaultConfig} = require('expo/metro-config')
+const path = require('path')
+
+const projectRoot = __dirname
+const monorepoRoot = path.resolve(projectRoot, '../..')
+
+const config = getDefaultConfig(projectRoot)
+
+// Watch all files in the monorepo
+config.watchFolders = [monorepoRoot]
+
+// Let Metro know where to resolve packages
+config.resolver.nodeModulesPaths = [
+  path.resolve(projectRoot, 'node_modules'),
+  path.resolve(monorepoRoot, 'node_modules'),
+]
+
+module.exports = config

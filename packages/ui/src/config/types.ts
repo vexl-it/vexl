ba2e0d736b0c1842500f0ace98e1926b34d18b1d
SamTremko <samueltremko@gmail.com>
2026-03-04 18:56:11 +0100

    fix: issues after rebase

--- packages/ui/src/config/types.ts
+++ packages/ui/src/config/types.ts
@@ -3,6 +3,5 @@ import type {config} from './tamagui.config'
 export type AppConfig = typeof config
 
 declare module 'tamagui' {
-  // eslint-disable-next-line @typescript-eslint/no-empty-interface
   interface TamaguiCustomConfig extends AppConfig {}
 }
--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -3,7 +3,7 @@ export {bodyFont, headingFont} from './config/fonts'
 export {config, config as tamaguiConfig} from './config/tamagui.config'
 export {darkTheme, lightTheme} from './config/themes'
 export {tokens} from './config/tokens'
-export type {} from './config/types'
+export type {AppConfig} from './config/types'
 
 // Provider
 export {useThemeMode, type ThemeMode} from './provider/useThemeMode'

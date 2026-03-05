16210e10afa4d852116181b2f9fe18d309fa97fe
SamTremko <samueltremko@gmail.com>
2026-03-05 13:17:16 +0100

    feat: Add the containing Screen component

--- apps/mobile/src/navigationTypes.ts
+++ apps/mobile/src/navigationTypes.ts
@@ -102,6 +102,8 @@ export type RootStackParamsList = {
   JoinClubFlow: NavigatorScreenParams<JoinClubFlowParamsList>
 
   BlogArticlesList: undefined
+
+  Settings: undefined
 }
 
 // eslint-disable-next-line @typescript-eslint/consistent-type-definitions

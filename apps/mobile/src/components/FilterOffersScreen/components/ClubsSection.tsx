4e4c7339d6b7139f0d30ffc8fb36be3dbcc69f6e
SamTremko <samueltremko@gmail.com>
2026-04-09 17:41:11 +0200

    feat #2191: Filters screen and logic

---
+++ apps/mobile/src/components/FilterOffersScreen/components/ClubsSection.tsx
@@ -0,0 +1,59 @@
+import {Avatar, SelectClubCell} from '@vexl-next/ui'
+import {YStack} from '@vexl-next/ui/src/primitives'
+import {useAtom, useAtomValue, type Atom} from 'jotai'
+import React, {useMemo} from 'react'
+import {clubsWithMembersAtomsAtom} from '../../../state/clubs/atom/clubsWithMembersAtom'
+import {type ClubWithMembers} from '../../../state/clubs/domain'
+import atomKeyExtractor from '../../../utils/atomUtils/atomKeyExtractor'
+import {useTranslation} from '../../../utils/localization/I18nProvider'
+import {createSelectClubInFilterAtom} from '../atom'
+
+function FilterClubItem({
+  clubWithMembersAtom,
+}: {
+  clubWithMembersAtom: Atom<ClubWithMembers>
+}): React.ReactElement {
+  const {t} = useTranslation()
+  const {club, members} = useAtomValue(clubWithMembersAtom)
+  const selectAtom = useMemo(
+    () => createSelectClubInFilterAtom(clubWithMembersAtom),
+    [clubWithMembersAtom]
+  )
+  const [isSelected, setSelected] = useAtom(selectAtom)
+
+  const membersCount = Intl.NumberFormat().format(members.length)
+
+  return (
+    <SelectClubCell
+      name={club.name}
+      description={t('clubs.members', {membersCount})}
+      selected={isSelected}
+      avatar={
+        <Avatar
+          size="small"
+          customSize={40}
+          source={{uri: club.clubImageUrl}}
+        />
+      }
+      onPress={() => {
+        setSelected(!isSelected)
+      }}
+    />
+  )
+}
+
+function ClubsSection(): React.ReactElement | null {
+  const clubsWithMembersAtoms = useAtomValue(clubsWithMembersAtomsAtom)
+
+  if (clubsWithMembersAtoms.length === 0) return null
+
+  return (
+    <YStack gap="$3">
+      {clubsWithMembersAtoms.map((one) => (
+        <FilterClubItem key={atomKeyExtractor(one)} clubWithMembersAtom={one} />
+      ))}
+    </YStack>
+  )
+}
+
+export default ClubsSection

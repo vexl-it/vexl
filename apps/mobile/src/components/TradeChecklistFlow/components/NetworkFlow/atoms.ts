38f976fb78dd88d23b6c5a66ea585dd08deeb637
David Novák <mail@davenov.com>
2026-04-13 11:59:48 +0200

    Netwrok in trading checklist

--- apps/mobile/src/components/TradeChecklistFlow/components/NetworkFlow/atoms.ts
+++ apps/mobile/src/components/TradeChecklistFlow/components/NetworkFlow/atoms.ts
@@ -1,18 +1,45 @@
 import {type BtcNetwork} from '@vexl-next/domain/src/general/offers'
 import {BtcAddress} from '@vexl-next/domain/src/utility/BtcAddress.brand'
 import {Option, Schema} from 'effect/index'
-import {atom} from 'jotai'
+import {atom, type SetStateAction} from 'jotai'
+import getValueFromSetStateActionOfAtom from '../../../../utils/atomUtils/getValueFromSetStateActionOfAtom'
 import {addNetworkActionAtom} from '../../atoms/updatesToBeSentAtom'
 
 export const btcNetworkAtom = atom<BtcNetwork>('LIGHTING')
 export const btcAddressAtom = atom<BtcAddress | undefined>(undefined)
+export const btcAddressTempAtom = atom<string>('')
 export const displayParsingErrorAtom = atom<boolean>(false)
+export const btcAddressInputAtom = atom(
+  (get) => get(btcAddressTempAtom),
+  (get, set, update: SetStateAction<string>) => {
+    const nextValue = getValueFromSetStateActionOfAtom(update)(() =>
+      get(btcAddressTempAtom)
+    )
+
+    if (!nextValue) {
+      set(displayParsingErrorAtom, false)
+      set(btcAddressTempAtom, nextValue)
+      return
+    }
+
+    const parsingResult = Schema.decodeUnknownOption(BtcAddress)(nextValue)
+
+    if (Option.isSome(parsingResult)) {
+      set(displayParsingErrorAtom, false)
+    } else {
+      set(displayParsingErrorAtom, true)
+    }
+
+    set(btcAddressTempAtom, nextValue)
+  }
+)
 
 export const saveBtcAddressActionAtom = atom(
   null,
   (get, set, btcAddress: string) => {
     if (!btcAddress) {
       set(btcAddressAtom, undefined)
+      set(displayParsingErrorAtom, false)
       return true
     }
 
@@ -32,7 +59,8 @@ export const saveLocalNetworkStateToMainStateActionAtom = atom(
   null,
   (get, set) => {
     const btcNetwork = get(btcNetworkAtom)
-    const btcAddress = get(btcAddressAtom)
+    const btcAddress =
+      btcNetwork === 'ON_CHAIN' ? get(btcAddressAtom) : undefined
 
     set(addNetworkActionAtom, {btcNetwork, btcAddress})
   }

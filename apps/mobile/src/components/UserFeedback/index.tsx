9792a5ec408742134eaabdb990d08795ce85da19
David Novák <mail@davenov.com>
2026-05-02 16:27:59 +0200

    Chat ended flow

--- apps/mobile/src/components/UserFeedback/index.tsx
+++ apps/mobile/src/components/UserFeedback/index.tsx
@@ -6,23 +6,41 @@ import {ScopeProvider} from 'bunshi/dist/react'
 import {atom} from 'jotai'
 import React, {useMemo} from 'react'
 import {FeedbackScope, generateInitialFeedback} from './atoms'
+import FeedbackBannerActions from './components/FeedbackBannerActions'
 import FeedbackBannerContent from './components/FeedbackBannerContent'
+import {
+  showUserFeedbackDialogAtom,
+  UserFeedbackDialog,
+  type UserFeedbackResult,
+} from './UserFeedbackDialog'
+
+const empty = (userFeedbackResult: UserFeedbackResult): void => {}
 
 interface Props {
   feedbackType: FeedbackType
-  onFinishClose: () => void
+  onFinishClose?: (userFeedbackResult: UserFeedbackResult) => void
 }
 
-function UserFeedback({feedbackType, onFinishClose}: Props): React.ReactElement | null {
+function UserFeedback({
+  feedbackType,
+  onFinishClose,
+}: Props): React.ReactElement | null {
   const feedbackAtom = useMemo(() => {
     return atom<Feedback>(generateInitialFeedback(feedbackType))
   }, [feedbackType])
 
   return (
-    <ScopeProvider scope={FeedbackScope} value={feedbackAtom}>
-      <FeedbackBannerContent onFinishClose={onFinishClose} />
-    </ScopeProvider>
+    <>
+      <ScopeProvider scope={FeedbackScope} value={feedbackAtom}>
+        <FeedbackBannerContent />
+        <FeedbackBannerActions
+          hideCloseButton
+          onFinishClose={onFinishClose ?? empty}
+        />
+      </ScopeProvider>
+    </>
   )
 }
 
 export default UserFeedback
+export {showUserFeedbackDialogAtom, UserFeedbackDialog, type UserFeedbackResult}

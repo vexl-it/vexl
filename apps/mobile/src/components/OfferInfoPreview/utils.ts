9064222242784a96d9f21c8f8e7758ce8fbad79a
David Novák <mail@davenov.com>
2026-05-15 12:09:21 +0200

    Remove unused components

--- apps/mobile/src/components/OfferInfoPreview/utils.ts
+++
@@ -1,5 +0,0 @@
-import {type OfferLocation} from '@vexl-next/domain/src/general/offers'
-
-export const formatLocationForColumns = (
-  location: readonly OfferLocation[]
-): string => location.map((one) => `${one.address}`).join('; ')

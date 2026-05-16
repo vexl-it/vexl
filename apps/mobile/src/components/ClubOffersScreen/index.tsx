62368975c05041b56b60aa4c2927182be26ce3ce
David Novák <mail@davenov.com>
2026-05-16 12:19:19 +0200

    Fix: ui in offers list screen.

--- apps/mobile/src/components/ClubOffersScreen/index.tsx
+++ apps/mobile/src/components/ClubOffersScreen/index.tsx
@@ -1,55 +1,55 @@
+import {ChevronLeft, NavigationBar, Screen, Stack} from '@vexl-next/ui'
 import {Option, pipe} from 'effect'
 import {useAtomValue, useSetAtom} from 'jotai'
 import React from 'react'
-import {Stack, Text, YStack} from 'tamagui'
 import {type RootStackScreenProps} from '../../navigationTypes'
 import {loadingStateAtom} from '../../state/marketplace/atoms/loadingState'
 import {refreshOffersActionAtom} from '../../state/marketplace/atoms/refreshOffersActionAtom'
 import {useTranslation} from '../../utils/localization/I18nProvider'
+import useSafeGoBack from '../../utils/useSafeGoBack'
 import OffersList from '../OffersList'
-import Screen from '../Screen'
-import ScreenTitle from '../ScreenTitle'
 import {useClubOffersAtoms} from './state'
 
 type Props = RootStackScreenProps<'ClubOffers'>
 
-export function ClubOffersScreen({
-  navigation,
-  route,
-}: Props): React.ReactElement {
+export function ClubOffersScreen({route}: Props): React.ReactElement {
   const {clubOfferAtomAtoms, singleClubAtom} = useClubOffersAtoms(
     route.params.clubUuid
   )
   const {t} = useTranslation()
+  const safeGoBack = useSafeGoBack()
 
   const offersAtom = useAtomValue(clubOfferAtomAtoms)
   const club = useAtomValue(singleClubAtom)
   const loadingOffers = useAtomValue(loadingStateAtom).state === 'inProgress'
   const refreshOffers = useSetAtom(refreshOffersActionAtom)
+  const title = pipe(
+    club,
+    Option.map((club) => club.club.name),
+    Option.getOrElse(() => t('filterOffers.offers'))
+  )
 
   return (
-    <Screen>
-      <YStack f={1}>
-        <Stack mx="$4" my="$4">
-          <ScreenTitle
-            withBackButton
-            text={`${pipe(
-              club,
-              Option.map((club) => club.club.name),
-              Option.getOrElse(() => t('filterOffers.offers'))
-            )}`}
-          >
-            <Text></Text>
-          </ScreenTitle>
-        </Stack>
-        <Stack mx="$2" f={1}>
-          <OffersList
-            offersAtoms={offersAtom}
-            onRefresh={refreshOffers}
-            refreshing={loadingOffers}
-          />
-        </Stack>
-      </YStack>
+    <Screen
+      noHorizontalPadding
+      navigationBar={
+        <NavigationBar
+          style="back"
+          title={title}
+          leftAction={{
+            icon: ChevronLeft,
+            onPress: safeGoBack,
+          }}
+        />
+      }
+    >
+      <Stack f={1}>
+        <OffersList
+          offersAtoms={offersAtom}
+          onRefresh={refreshOffers}
+          refreshing={loadingOffers}
+        />
+      </Stack>
     </Screen>
   )
 }

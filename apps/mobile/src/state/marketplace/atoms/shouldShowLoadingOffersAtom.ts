21ef4e678fb5eeaa102839b98f950ace23e43a9d
SamTremko <samueltremko@gmail.com>
2026-05-29 09:54:47 +0200

    fix: empty marketplace states

--- apps/mobile/src/state/marketplace/atoms/shouldShowLoadingOffersAtom.ts
+++ apps/mobile/src/state/marketplace/atoms/shouldShowLoadingOffersAtom.ts
@@ -4,19 +4,16 @@ import {reachNumberAtom} from '../../connections/atom/connectionStateAtom'
 import {importedContactsCountAtom} from '../../contacts/atom/contactsStore'
 import {notificationsEnabledAtom} from '../../notifications/areNotificationsEnabledAtom'
 import {REACH_NUMBER_THRESHOLD} from '../domain'
-import {areThereAnyMyOffersAtom} from './myOffers'
 
 export const shouldShowLoadingOffersAtom = atom((get) => {
   const importedContactsCount = get(importedContactsCountAtom)
   const reachNumber = get(reachNumberAtom)
   const areNotificationsEnabled = get(notificationsEnabledAtom)
-  const areThereAnyMyOffers = get(areThereAnyMyOffersAtom)
 
   return (
     importedContactsCount > 0 &&
     reachNumber >= REACH_NUMBER_THRESHOLD &&
     Option.isSome(areNotificationsEnabled) &&
-    areNotificationsEnabled.value.notifications &&
-    areThereAnyMyOffers
+    areNotificationsEnabled.value.notifications
   )
 })

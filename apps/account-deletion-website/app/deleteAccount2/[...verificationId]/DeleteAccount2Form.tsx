29b1a819dd8baa95638d8a531ce1e26f328206ad
David Novák <mail@davenov.com>
2026-06-01 13:30:26 +0200

    address greptile review feedback

--- apps/account-deletion-website/app/deleteAccount2/[...verificationId]/DeleteAccount2Form.tsx
+++ apps/account-deletion-website/app/deleteAccount2/[...verificationId]/DeleteAccount2Form.tsx
@@ -3,7 +3,7 @@
 import SubmitButton from '@/src/components/SubmitButton'
 import {emptyErrorFormState} from '@/src/shared/formState'
 import Link from 'next/link'
-import {useActionState, useEffect, useState} from 'react'
+import {useActionState} from 'react'
 import {submitDeleteAccount2} from './actions'
 
 export default function DeleteAccount2Form({
@@ -15,11 +15,6 @@ export default function DeleteAccount2Form({
     submitDeleteAccount2,
     emptyErrorFormState
   )
-  const [debugData, setDebugData] = useState(false)
-
-  useEffect(() => {
-    setDebugData(window.debugData === true)
-  }, [])
 
   return (
     <form action={formAction} id="input-number">
@@ -30,19 +25,19 @@ export default function DeleteAccount2Form({
           className="input-field"
           name="code"
           required
-          aria-label="Your phone number with prefix"
-          type="tel"
+          aria-label="Verification code from SMS"
+          type="text"
+          inputMode="numeric"
           placeholder="code from message"
         />
       </label>
       <Link className="block-align-end" href="/deleteAccount1">
-        Resent
+        Resend
       </Link>
-      <input type="hidden" name="debugData" value={String(debugData)} />
       <input type="hidden" name="verificationId" value={verificationId} />
       <p>
         You are about to delete your account. This action is irreversible. Do
-        really you want to delete your account?
+        you really want to delete your account?
       </p>
       <SubmitButton label="Yes delete" />
     </form>

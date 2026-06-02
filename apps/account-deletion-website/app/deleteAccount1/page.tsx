b4920946edfbebcf7a2c5132f2001b06b9e5e816
David Novák <mail@davenov.com>
2026-06-02 10:47:54 +0200

    feat: Use process turnstile key, not build time env var

--- apps/account-deletion-website/app/deleteAccount1/page.tsx
+++ apps/account-deletion-website/app/deleteAccount1/page.tsx
@@ -1,13 +1,19 @@
+import {connection} from 'next/server'
 import DeleteAccount1Form from './DeleteAccount1Form'
 
-export default function DeleteAccount1Page() {
+export default async function DeleteAccount1Page() {
+  await connection()
+
+  const turnstileSiteKey =
+    process.env.TURNSTILE_SITE_KEY ?? process.env.NEXT_PUBLIC_TURNSTILE_SITE_KEY
+
   return (
     <div>
       <p>
         To delete your account, login with the phone number you used in the Vexl
         app.
       </p>
-      <DeleteAccount1Form />
+      <DeleteAccount1Form turnstileSiteKey={turnstileSiteKey} />
     </div>
   )
 }

33ddc78aae26fc8fa4707263069239b42b3d26f3
David Novák <mail@davenov.com>
2026-06-03 11:47:32 +0200

    fix: release 1.43.1 (750)

--- apps/mobile/app.config.ts
+++ apps/mobile/app.config.ts
@@ -1,5 +1,5 @@
-const VERSION_CODE = 745
-const VERSION = '1.43.0'
+const VERSION_CODE = 750
+const VERSION = '1.43.1'
 const ENV_PRESET = process.env.ENV_PRESET
 const IS_LOCAL_ENV = ENV_PRESET === 'local'
 const COMMIT_HASH = process.env.EAS_BUILD_GIT_COMMIT_HASH ?? 'local'

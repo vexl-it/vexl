04b3be11e09c10f02aef19f4f172a438f1108015
SamTremko <samueltremko@gmail.com>
2026-06-28 17:22:50 +0200

    fix: preserve product category suggestion dismissal

--- apps/mobile/src/state/marketplace/atoms/offerSuggestionVisible.ts
+++ apps/mobile/src/state/marketplace/atoms/offerSuggestionVisible.ts
@@ -204,11 +204,3 @@ export const dismissMissingProductCategoriesInMarketplaceSuggestionActionAtom =
     set(missingProductCategoriesSuggestionDismissedThisSessionAtom, true)
     set(anyMarketplaceSuggestionDismissedInThisSessionAtom, true)
   })
-
-export const resetImportContactsMarketplaceSuggestionSessionActionAtom = atom(
-  null,
-  (get, set) => {
-    set(anyMarketplaceSuggestionDismissedInThisSessionAtom, false)
-    set(missingProductCategoriesSuggestionDismissedThisSessionAtom, false)
-  }
-)

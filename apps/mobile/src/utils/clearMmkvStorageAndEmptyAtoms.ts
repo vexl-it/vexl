089a716b907a9b66ea1a69fb0460a569acaef650
SamTremko <samueltremko@gmail.com>
2026-07-01 17:55:37 +0200

    chore: Add logs to monitor potential mmkv data loss

--- apps/mobile/src/utils/clearMmkvStorageAndEmptyAtoms.ts
+++ apps/mobile/src/utils/clearMmkvStorageAndEmptyAtoms.ts
@@ -1,9 +1,12 @@
+import AsyncStorage from '@react-native-async-storage/async-storage'
 import {CLEAR_STORAGE_KEY} from './atomUtils/atomWithParsedMmkvStorage'
-import {storage} from './mmkv/effectMmkv'
+// TODO: ASYNC_SENTINEL_KEY import is part of temporary MMKV data loss diagnostic. Remove together with the sentinel.
+import {ASYNC_SENTINEL_KEY, storage} from './mmkv/effectMmkv'
 
 export default function clearMmkvStorageAndEmptyAtoms(): void {
   // set all atoms to defaultValue
   storage._storage.set(CLEAR_STORAGE_KEY, Date.now().toString())
 
+  void AsyncStorage.removeItem(ASYNC_SENTINEL_KEY).catch(() => {})
   storage._storage.clearAll()
 }

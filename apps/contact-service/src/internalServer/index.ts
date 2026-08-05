40a857d35ca2e8279aa5d34edf3f56b5cd80801e
David Novák <kaladivo@gmail.com>
2026-08-05 13:56:09 +0200

    feat: move deactivate-and-clear-clubs from internal endpoint to repeating task

--- apps/contact-service/src/internalServer/index.ts
+++ apps/contact-service/src/internalServer/index.ts
@@ -6,21 +6,10 @@ import {
 import {makeInternalServer} from '@vexl-next/server-utils/src/InternalServer'
 import {internalServerPortConfig} from '@vexl-next/server-utils/src/commonConfigs'
 import {Effect, Schema} from 'effect'
-import {deactivateAndClearClubs} from './routes/deactivateAndClearClubs'
 import {testHasingSpeed} from './routes/testHashingSpeed'
 
 export const internalServerLive = makeInternalServer(
   HttpRouter.empty.pipe(
-    HttpRouter.post(
-      '/deactivate-and-clear-clubs',
-      deactivateAndClearClubs.pipe(
-        Effect.mapBoth({
-          onFailure: (error) =>
-            HttpServerResponse.text(error._tag, {status: 500}),
-          onSuccess: () => HttpServerResponse.text('ok', {status: 200}),
-        })
-      )
-    ),
     HttpRouter.post(
       '/test-hashing-speed',
       Effect.gen(function* (_) {
